87790c383428b93bb21a58ea38665e53b5191b76
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-06-29 21:33:09 -0700

    ok so far, converting from O(N^2) to O(N)

--- USACOClassLiveTest/src/cownomics.java
+++ USACOClassLiveTest/src/cownomics.java
@@ -5,21 +5,65 @@ public class cownomics {
 
 	public static void main(String[] args) throws IOException {
 		// IO
-		//                                    new FileReader("cownomics.in")
+		// new FileReader("cownomics.in")
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-		//                                    new BufferedWriter(new FileWriter("cownomics.out"))
-		int N, M; 
-		StringTokenizer st =new StringTokenizer(f.readLine());
+		// new BufferedWriter(new FileWriter("cownomics.out"))
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+		int N, M;
+		StringTokenizer st = new StringTokenizer(f.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		String[] spotty = new String[N];
-		String[] spotless = new String[N];
-		for(int i = 0; i < N; i ++) {
-			spotty[i] = f.readLine();
+		// String[] spotty = new String[N];
+		// String[] spotless = new String[N];
+		boolean[][] spottySeen = new boolean[M][4];
+		boolean[][] spotlessSeen = new boolean[M][4];
+		for (int i = 0; i < N; i++) {
+			String line = f.readLine();
+			for (int j = 0; j < M; j++) {
+				char c = line.charAt(j);
+				switch (c) {
+				case 'G':
+					spottySeen[j][0] = true;
+					break;
+				case 'A':
+					spottySeen[j][1] = true;
+					break;
+
+				case 'C':
+					spottySeen[j][2] = true;
+					break;
+
+				case 'T':
+					spottySeen[j][3] = true;
+					break;
+
+				}
+			}
 		}
-		for(int i = 0; i < N; i ++) {
-			spotless[i] = f.readLine();
+		for (int i = 0; i < N; i++) {
+			String line = f.readLine();
+			for (int j = 0; j < M; j++) {
+				char c = line.charAt(j);
+				switch (c) {
+				case 'G':
+					spotlessSeen[j][0] = true;
+					break;
+				case 'A':
+					spotlessSeen[j][1] = true;
+					break;
+
+				case 'C':
+					spotlessSeen[j][2] = true;
+					break;
+
+				case 'T':
+					spotlessSeen[j][3] = true;
+					break;
+
+				}
+			}
 		}
+		
 	}
 
 }
--- USACOClassLiveTest/src/diamond.java
+++ USACOClassLiveTest/src/diamond.java
@@ -4,25 +4,80 @@ public class diamond {
 
 	public static void main(String[] args) throws IOException{
 		// IO
-		//                                    new FileReader("diamond.in")
-		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-		//                               new BufferedWriter(new FileWriter("diamond.out"))
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+		BufferedReader f = new BufferedReader(new FileReader("diamond.in"));
+		//BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("diamond.out")));
+		//PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(f.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		List<Integer> diamonds = new ArrayList<>();
+		List<Integer> diamonds = new ArrayList<>(N);
 		for(int i = 0 ; i < N; i ++) {
-			diamonds.add(Integer.parseInt(st.nextToken()));
+			diamonds.add(Integer.parseInt(f.readLine()));
 		}
-		int idx = 0;
-		for(int i =0 ; i < N-1; i ++) {
-			//.while((diamonds.get(i) - )) {
-				
-			//}
-		}
+		List<Result> results = new ArrayList<>(N);
 		diamonds.sort(null);
-		
+		//int id = 0;
+		int toprange = 0;
+		for(int i =0 ; i < N; i ++) {
+			int dia = diamonds.get(i);
+			while(toprange < N && (diamonds.get(toprange) - dia <= K)) {
+				toprange++;
+			}
+			results.add(new Result(i, toprange));
+		}
+		//System.out.println(results);
+		//System.out.println(results.size());
+		int best = 0;
+		int Nm = N-1;
+		for(int i =0 ;i < Nm; i ++) {
+			int max1 = -1;
+			int max2 = -1;
+			Result r = new Result(-1,-1);// Make compiler happy
+			for(int j =0 ;j <= i; j ++) {
+				r = results.get(j);
+				int selected = r.end - r.start;
+				if(selected > max1) {
+					max1 = selected;
+				}
+			}
+			Result s;
+			for(int j =i+1; j<N; j ++) {
+				s = results.get(j);
+				if(s.start < r.end) {
+					continue;
+				}
+				int selected = s.end - s.start;
+				//System.out.println(selected);
+				if(selected > max2) {
+					max2 = selected;
+				}
+			}
+			//System.out.println(max1 +"   "+ max2);
+			if((max1 + max2) > best) {
+				best = max1 + max2;
+			}
+		}
+		pw.println(best);
+		pw.close();
+		f.close();
 	}
 
+}
+class Result{
+	int start = 0, end = 0;// SATSIFY COMPILER
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Result [start=");
+		builder.append(start);
+		builder.append(", end=");
+		builder.append(end);
+		builder.append("]");
+		return builder.toString();
+	}
+	public Result(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
 }
