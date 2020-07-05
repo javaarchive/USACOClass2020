cbfbe159745aedfebddc3074c20d42d44f21a865
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-04 21:55:54 -0700

    sort major change

--- USACOClassLiveTest/src/fairphoto.java
+++ USACOClassLiveTest/src/fairphoto.java
@@ -1,7 +1,26 @@
 import java.io.*;
 import java.util.*;
 public class fairphoto{
-
+	static int[][] ps;
+	public static int size, HCowcount, GCowcount, balance;
+	public static void query(int start, int end){
+		if(start == 0){
+			size = ps[end][3];
+			HCowcount = ps[end][2];
+			GCowcount = ps[end][1];
+			balance = ps[end][0];
+		}else{
+			int startm1 = start;
+			size = ps[end][3] - ps[startm1][3];
+			HCowcount = ps[end][2] - ps[startm1][2];
+			GCowcount = ps[end][1] - ps[startm1][1];
+			balance = ps[end][0] - ps[startm1][0];
+		}
+	}
+	public static boolean valid(){
+		return (GCowcount == 0) || (HCowcount == 0) || (HCowcount == GCowcount);
+	}
+	static int seen[];
 	public static void main(String[] args) throws IOException{
 		// IO
 		//                                    new FileReader("cownomics.in")
@@ -16,6 +35,7 @@ public class fairphoto{
 			st = new StringTokenizer(f.readLine());
 			cows.add(new Livestock(Integer.parseInt(st.nextToken()), st.nextToken().charAt(0)));
 		}
+		seen = new int[2*N + 1];
 		cows.sort(new Comparator<Livestock>() {
 			@Override
 			public int compare(Livestock arg0, Livestock arg1) {
@@ -25,27 +45,34 @@ public class fairphoto{
 		});
 		System.out.println(cows);
 		Livestock cow;
-		int[][] ps = new int[N][3];
+		ps = new int[N][4];
+		Arrays.fill(seen, -1);
 		int bal = 0;
-		int lastH = -1
-		int lastG = -1;
+		int Hcount = 0;
+		int Gcount = 0;
 		for(int i = 0; i < cows.size(); i ++) {
 			cow = cows.get(i);
 			bal += cow.type ? -1: 1;
 			if(cow.type == true){
-				lastG = i;
+				Gcount ++;
 			}else{
-				lastH = i;
+				Hcount ++;
 			}
 			ps[i][0] = bal;
-			ps[i][1] = lastH;
-			ps[i][2] = lastG;
+			/*ps[i][1] = Hcount;
+			ps[i][2] = Gcount;
+			ps[i][3] = cow.index;*/
+			seen[(bal+N+1)] = i;
+		}
+		for(int i = N; i >= 0; i --){
+			
 		}
 		pw.close();
 		f.close();
 		
 	}
 }
+
 class Livestock{
 	boolean type;
 	int index;
