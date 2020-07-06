6ffe7b7e604404b780ef0b2bd08dc1bcabdb6c98
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-05 21:23:59 -0700

    scramble base finished

--- USACOClassLiveTest/src/scramble.java
+++ USACOClassLiveTest/src/scramble.java
@@ -1,12 +1,26 @@
 import java.io.*;
 import java.util.*;
 public class scramble {
+    public static int numberize(char c){
+        return Character.getNumericValue(c) - 10;
+    }
     public static void main(String[] args) throws IOException{
-		// IO
+        // IO
         //BufferedReader f = new BufferedReader(new FileReader("reduce.in"));
         BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out)));
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         //PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("reduce.out")));
-        //System.out.println(Cha)
+        int N = Integer.parseInt(f.readLine());
+        for(int i = 0; i < N; i ++){
+            String line = f.readLine();
+            
+        }
+    }
+}
+class ScrambledCow{
+    String str;
+    public int minpos = -1, maxpos = -1;
+    public ScrambledCow(String s){
+        this.str = s;
     }
 }
