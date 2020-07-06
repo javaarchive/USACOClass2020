a1a7b81d30415fac423e427239fd1e2c7d158940
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-05 20:02:48 -0700

    create stacking base

--- USACOClassLiveTest/src/stacking.java
+++ USACOClassLiveTest/src/stacking.java
@@ -8,9 +8,19 @@ public class stacking{
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
 		//                               new BufferedWriter(new FileWriter("cownomics.out"))
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-		int N, M; 
-		
-		
+		int N, K; 
+		StringTokenizer st = new StringTokenizer(f.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		int[] starts = new int[K];
+		int[] ends = new int[K];
+		for(int i = 0; i < N; i ++){
+			st = new StringTokenizer(f.readLine());
+			starts[i] = Integer.parseInt(st.nextToken());
+			ends[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(starts);
+		Arrays.sort(ends);
 	}
 
 }
