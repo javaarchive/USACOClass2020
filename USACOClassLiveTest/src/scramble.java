1e50dfd280d6647ccb33a11de75fd78ef43ca264
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-06 20:49:50 -0700

    debugging

--- USACOClassLiveTest/src/scramble.java
+++ USACOClassLiveTest/src/scramble.java
@@ -1,26 +1,87 @@
 import java.io.*;
 import java.util.*;
 public class scramble {
-    public static int numberize(char c){
+    public static int numberize(final char c) {
         return Character.getNumericValue(c) - 10;
     }
-    public static void main(String[] args) throws IOException{
+
+    public static String sortString(final String s, final int mult) {
+        final Character[] carr = s.chars().mapToObj(c -> (char)c).toArray(Character[]::new); 
+        Arrays.sort(carr, new Comparator<Character>() {
+            @Override
+            public int compare(final Character o1, final Character o2) {
+                // TODO Auto-generated method stub
+                return mult * Character.compare(o1, o2);
+            }
+        });
+        String str = "";
+        for (Character c : carr)
+            str += c.toString();
+        System.out.println(str);
+        return str;        
+        //return new StringBuild
+    }
+
+    public static void main(final String[] args) throws IOException {
         // IO
-        //BufferedReader f = new BufferedReader(new FileReader("reduce.in"));
-        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-        //PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("reduce.out")));
-        int N = Integer.parseInt(f.readLine());
-        for(int i = 0; i < N; i ++){
-            String line = f.readLine();
-            
+        // BufferedReader f = new BufferedReader(new FileReader("reduce.in"));
+        final BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+        final PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+        // PrintWriter pw = new PrintWriter(new BufferedWriter(new
+        // FileWriter("reduce.out")));
+        final int N = Integer.parseInt(f.readLine());
+        final List<ScrambledCow> cows = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            final String line = f.readLine();
+            cows.add(new ScrambledCow(line, i));
         }
+        cows.sort(new Comparator<ScrambledCow>() {
+            @Override
+            public int compare(final ScrambledCow o1, final ScrambledCow o2) {
+                return sortString(o1.str, -1).compareTo(sortString(o2.str, 1));
+            }
+
+        });
+        System.out.println(cows);
+        for(int i = 0; i < N; i ++){
+            cows.get(i).minpos = i + 1;
+        }
+        cows.sort(new Comparator<ScrambledCow>() {
+            @Override
+            public int compare(final ScrambledCow o1, final ScrambledCow o2) {
+                return sortString(o1.str, 1).compareTo(sortString(o2.str, -1));
+            }
+
+        });
+        System.out.println(cows);
+        for(int i = 0; i < N; i ++){
+            cows.get(i).maxpos = i + 1;
+        }
+        cows.sort(new Comparator<ScrambledCow>() {
+            @Override
+            public int compare(final ScrambledCow o1, final ScrambledCow o2) {
+                return Integer.compare(o1.origIndex, o2.origIndex);
+            }
+
+        });
+        for(int i = 0; i < N; i ++){
+            ScrambledCow c = cows.get(i);
+            pw.println(c.minpos+" "+c.maxpos);
+        }
+        System.out.println(cows);
+        pw.close();
     }
 }
-class ScrambledCow{
+
+class ScrambledCow {
     String str;
     public int minpos = -1, maxpos = -1;
-    public ScrambledCow(String s){
+    int origIndex = 0;
+    public ScrambledCow(final String s, int origIndex) {
+        this.origIndex = origIndex;
         this.str = s;
+    }
+    public String toString(){
+        return "("+this.minpos+"-"+this.maxpos+":"+this.str+")";
     }
 }
