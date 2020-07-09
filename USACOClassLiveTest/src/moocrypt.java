bbae953945f3be01a3a3a57f56e033ef69ea7ed5
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-08 21:21:27 -0700

    finish moocrypt

--- USACOClassLiveTest/src/moocrypt.java
+++ USACOClassLiveTest/src/moocrypt.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-
 public class moocrypt {
 
 	public static void main(String[] args) throws IOException{
@@ -19,39 +18,52 @@ public class moocrypt {
 		map = new int[rows][cols];
 		for(int i = 0; i < rows; i ++) {
 			line = f.readLine();
-			for(int j = 0; j < rows; j ++) {
+			for(int j = 0; j < cols; j ++) {
 				map[i][j] = (int) line.charAt(j);
 			}
 		}
 		int total = 0;
-		int start = (int) 'a';
-		int end = (int) 'z' + 1;
-		for(int i = 0; i < dir.length; i ++) {
-			for(int a = start; a < end; a ++) {
-				for(int b = start; b < end; b ++) {
-					if(a == b) {
+		int start = (int) 'A';
+		int end = (int) 'Z';
+		int best = -1;
+		//System.out.println(Arrays.deepToString(map));
+			for(int a = start; a <= end; a ++) {
+				if(a == 'M'){
+					continue;
+				}
+				for(int b = start; b <= end; b ++) {
+					if(a == b || b == 'O') {
 						continue;
 					}
+					total = 0;
+					//boolean[][] used = new boolean[rows][cols];
+					for(int i = 0; i < dir.length; i ++) {
 					for(int row = 0; row < rows; row ++) {
 						for(int col = 0; col < cols; col ++) {
 							int curX = row;
 							int curY = col;
-							System.out.println(curX+" "+curY+" "+Arrays.toString(dir[i]));
+							//System.out.println(i+" "+a+" "+b+" "+curX+" "+curY+" "+Arrays.toString(dir[i]));
 							boolean works = false;
+							boolean inBounds = 0 <= (curX + 2 * dir[i][0]) && 0 <= (curY + 2 * dir[i][1]) && (curX + 2 * dir[i][0]) < rows && (curY + 2 * dir[i][1]) < cols;
+							works = inBounds && (map[curX][curY] == a) 
+							&& (map[curX+dir[i][0]][curY+dir[i][1]] == b) 
+							&& (map[curX+dir[i][0]+dir[i][0]][curY+dir[i][1]+dir[i][1]] == b);
 							
-							works = (map[curX][curY] == a) && (map[curX+dir[i][0]][curY+dir[i][1]] == b) && (map[curX+dir[i][0]+dir[i][0]][curY+dir[i][1]+dir[i][1]] == b);
-							if(0 < curX + dir[i][0] || ) {
-								
-							}
 							if(works) {
+								//used[curX][curY] = true;
+								//System.out.println((char) a+" "+(char) b+" "+row+" "+col+" going "+dir[i][0]+" and "+dir[i][1]);
 								total++;
 							}
 						}
 					}
 				}
+					if(total > best){
+						best = total;
+					}
+				}
 			}
-		}
-		pw.println(total);
+		
+		pw.println(best);
 		pw.close();
 		f.close();
 	}
