807e3efd91987c00533c1146e6dfa996f1ae6e27
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-13 20:26:04 -0700

    linear scan approach

--- USACOClassLiveTest/src/badhair.java
+++ USACOClassLiveTest/src/badhair.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class bronzelilypad{
+public class badhair{
 
 	public static void main(String[] args) throws IOException{
 		// IO
@@ -8,8 +8,23 @@ public class bronzelilypad{
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
 		//                               new BufferedWriter(new FileWriter("cownomics.out"))
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-		int N, M; 
-		
+		int N = Integer.parseInt(f.readLine());
+		int heightPos = -1;
+		int bestHeight = -1;
+		int ans = 0;
+		for(int i = 0; i < N; i ++){
+			int height = Integer.parseInt(f.readLine());
+			if(height >= bestHeight){
+				if(heightPos != -1){
+					ans += i - heightPos - 1;
+				}
+				bestHeight = height;
+				heightPos = i;
+			}
+		}
+		ans += N - heightPos - 1;
+		pw.println(ans);
+		pw.close();
 		
 	}
 
