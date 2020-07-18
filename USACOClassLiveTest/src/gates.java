0d59bf2ce3f6680750af161ae07f4f34c960835b
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-17 22:05:06 -0700

    before revert

--- USACOClassLiveTest/src/gates.java
+++ USACOClassLiveTest/src/gates.java
@@ -7,13 +7,13 @@ public class gates {
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
 		//                               new BufferedWriter(new FileWriter("flyingcow.out"))
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-        boolean[][] grid = new boolean[4001][4001];
+        int[][] grid = new int[4001][4001];
         int N = Integer.parseInt(f.readLine());
         String line = f.readLine();
         int posX = 1001, posY = 1001;
-        grid[posX][posY] = true;
+        //grid[posX][posY] = ;
         int ans = 0;
-        boolean lastIntersected = false;
+        //boolean lastIntersected = false;
         //System.out.println("\u001B[31m");
         Place p = new Place(0,0);
         Set<Place> visited = new HashSet<>();
@@ -22,44 +22,54 @@ public class gates {
             char c = line.charAt(i);
             lastX = posX;
             lastY = posY;
+            int type = 0;
             switch(c){
                 case 'N':
                     posY ++;
                     posY ++;
+                    type = 1;
                     break;
                 case 'S':
                     posY --;
                     posY --;
+                    type = 1;
                     break;
                 case 'W':
                     posX --;
                     posX --;
+                    type = 2;
                     break;
                 case 'E':
                     posX ++;
                     posX ++;
+                    type = 2;
                     break;
             }
             int midX = (lastX+posX)/2, midY =  (posY+lastY)/2;
-            if(grid[midX][midY]){
+            if(grid[midX][midY] != 0 && grid[midX][midY] != type){
                 p.setCoord(midX, midY);
                 if(!visited.contains(p)){
                     ans ++;
                     visited.add(new Place(p.x, p.y)); // Clone
+                    //lastIntersected = true;
                 }
-            }
-            grid[lastX][lastY] = true;
-            grid[midX][midY] = true;
-            grid[posX][posY] = true;
+            }else{
+                //lastIntersected = false;
+        }
+            
+            //grid[lastX][lastY] = type;
+            grid[midX][midY] = type;
+            //grid[posX][posY] = type;
         }
         //grid[1001][1001] = false;
-        for(int i = 996; i < 1110; i ++){
+        /*for(int i = 996; i < 1110; i ++){
             for(int j = 999; j < 1100; j ++){
                 System.out.print(((grid[i][j])?"\u001B[33m":"\u001B[37m")+(grid[i][j] ? 1 : 0 )+"\u001B[0m");
             }
             System.out.println();
-        }
+        }*/
         pw.println(ans);
+
 		pw.close();
 		f.close();
 	}
