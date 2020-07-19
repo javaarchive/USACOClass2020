3609305128992162e397da7b0af3d508f73afb68
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-18 17:40:49 -0700

    reverting

--- USACOClassLiveTest/src/gates.java
+++ USACOClassLiveTest/src/gates.java
@@ -7,105 +7,46 @@ public class gates {
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
 		//                               new BufferedWriter(new FileWriter("flyingcow.out"))
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-        int[][] grid = new int[4001][4001];
+        int[][] grid = new int[2001][2001];
         int N = Integer.parseInt(f.readLine());
         String line = f.readLine();
         int posX = 1001, posY = 1001;
-        //grid[posX][posY] = ;
-        int ans = 0;
-        //boolean lastIntersected = false;
-        //System.out.println("\u001B[31m");
-        Place p = new Place(0,0);
-        Set<Place> visited = new HashSet<>();
-        int lastX, lastY;
-        char sc = line.charAt(0);
-        /*switch(sc){
-            case 'N':
-                posY --;
-                break;
-            case 'S':
-                posY ++;
-                break;
-            case 'W':
-                posX ++;
-                break;
-            case 'E':
-                posX --;
-                break;
-        }*/
-        int stype = 0;
-        switch(sc){
-            case 'S':
-            case 'N':
-                stype = 1;
-                break;
-            case 'W':
-            case 'E':
-                stype = 2;
-                break;
-        }
-        grid[posX][posY] = stype;
-        for(int i = 0 ; i < N; i ++){
+        List<List<Integer>> adjlist = new ArrayList<>();
+        int lastNode = 0;
+        adjlist.add(new ArrayList<>());
+        for(int i = 0; i < N; i ++){
             char c = line.charAt(i);
-            lastX = posX;
-            lastY = posY;
-            //System.out.println("X "+" "+grid[posX][posY]+" "+posX+" "+posY);
-            int type = 0;
+            int prevX = posX, prevY = posY;
             switch(c){
                 case 'N':
                     posY ++;
-                    posY ++;
-                    type = 1;
                     break;
                 case 'S':
                     posY --;
-                    posY --;
-                    type = 1;
                     break;
                 case 'W':
                     posX --;
-                    posX --;
-                    type = 2;
                     break;
                 case 'E':
                     posX ++;
-                    posX ++;
-                    type = 2;
                     break;
             }
-            int midX = (lastX+posX)/2, midY =  (posY+lastY)/2;
-            //System.out.println(type+" "+grid[posX][posY]+" "+posX+" "+posY);
-            if(grid[posX][posY] != 0 && grid[posX][posY] != type){
-                p.setCoord(posX, posY);
-                if(!visited.contains(p)){
-                    ans ++;
-                    visited.add(new Place(p.x, p.y)); // Clone
-                    //lastIntersected = true;
+            int node = grid[posX][posY];
+            if(node == 0){
+                grid[posX][posY] = adjlist.size();
+                if(lastNode >= adjlist.size()){
+                    adjlist.add(new ArrayList<>());
                 }
+                adjlist.get(lastNode).add(grid[posX][posY]);
+                lastNode = grid[posX][posY];
             }else{
-                //lastIntersected = false;
+                lastNode = grid[posX][posY];
             }
-            //grid[lastX][lastY] = type;
-            grid[midX][midY] = type;
-            //grid[posX][posY] = type;
         }
-        //grid[1001][1001] = false;
-        /*for(int i = 996; i < 1110; i ++){
-            for(int j = 999; j < 1100; j ++){
-                System.out.print(((grid[i][j])?"\u001B[33m":"\u001B[37m")+(grid[i][j] ? 1 : 0 )+"\u001B[0m");
-            }
-            System.out.println();
-        }*/
-        /*for(int i = 996; i < 1050; i ++){
-            for(int j = 999; j < 1050; j ++){
-                System.out.print(((grid[i][j] > 0)?"\u001B[33m":"\u001B[37m")+grid[i][j]+"\u001B[0m");
-            }
-            System.out.println();
-        }*/
-        pw.println(ans);
+        System.out.println(adjlist);
+        f.close();
+        pw.println();
 
-		pw.close();
-		f.close();
 	}
 }
 class Place{
@@ -141,4 +82,4 @@ class Place{
         return true;
     }
     
-}
+}   
