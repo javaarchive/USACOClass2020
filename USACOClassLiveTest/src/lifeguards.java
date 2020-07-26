be2cb701f62bce627d045a7f9fa57a8463ca308a
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-25 17:14:59 -0700

    fixed it lol got pass with unoptimized code

--- USACOClassLiveTest/src/lifeguards.java
+++ USACOClassLiveTest/src/lifeguards.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+
 public class lifeguards {
     public static void main(String[] args) throws IOException {
 		// IO
@@ -9,14 +10,14 @@ public class lifeguards {
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(f.readLine());
         List<Interval> inters = new ArrayList<>(N);
-        Interval[][] lookup = new Interval[N][2];
+        Interval[][] lookup = new Interval[2][N];
         for(int i = 0 ; i < N; i ++){
             StringTokenizer st = new StringTokenizer(f.readLine());
             int a = Integer.parseInt(st.nextToken()),b = Integer.parseInt(st.nextToken());
             Interval il1 = new Interval(a, b, 1, i);
             Interval il2 = new Interval(a, b, 2, i);
-            lookup[i][0] = il1;
-            lookup[i][1] = il2;
+            lookup[0][i] = il1;
+            lookup[1][i] = il2;
             inters.add(il1);
             inters.add(il2);
         }
@@ -30,18 +31,20 @@ public class lifeguards {
             }
         });
         int curCount = 0;
-        int lastTime = 0;
+        int lastTime = inters.get(0).start;
         int lastID = 0;
         Set<Integer> cows = new HashSet<>();
+        int totalTime = 0;
         for(int i = 0; i < 2*N; i ++){
             Interval il = inters.get(i);
             int time = (il.type == 1) ? il.start: il.end;
             if(curCount == 1){
-                int selectedID = (il.type == 1) ? lastID: il.id;
-                System.out.println("Calc: "+time+" "+lastTime);
-                System.out.println(time- lastTime+" id: "+selectedID+" type: "+il.type);
-                lookup[selectedID][0].aloneTime += time- lastTime;
-                lookup[selectedID][1].aloneTime += time- lastTime;
+                //System.out.println("Last id"+lastID);
+                int selectedID = cows.toArray(new Integer[1])[0];
+                //System.out.println("Calc: "+time+" "+lastTime);
+                //System.out.println(time- lastTime+" id: "+selectedID+" type: "+il.type);
+                lookup[0][selectedID].aloneTime += time- lastTime;
+                lookup[1][selectedID].aloneTime += time- lastTime;
             }
             if(il.type == 1){
                 cows.add(il.id);
@@ -54,15 +57,58 @@ public class lifeguards {
             lastTime = time;
             lastID = il.id;
         }
-        System.out.println(inters);
+        //total = 0;
+        Arrays.sort(lookup[0],new Comparator<Interval>(){
+
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                // TODO Auto-generated method stub
+                return Integer.compare(o1.aloneTime, o2.aloneTime);
+            }
+
+        });
+        Arrays.sort(lookup[1],new Comparator<Interval>(){
+
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                // TODO Auto-generated method stub
+                return Integer.compare(o1.aloneTime, o2.aloneTime);
+            }
+
+        });
+        //System.out.println(Arrays.toString(lookup[0]));
+        //System.out.println("Removing "+lookup[1][0]);
+        inters.remove(Collections.binarySearch(inters, lookup[0][0]));
+        inters.remove(Collections.binarySearch(inters, lookup[1][0]));
+        for(int i = 0; i < inters.size()-1; i ++){
+            Interval i1 = inters.get(i);
+            Interval i2 = inters.get(i + 1);
+            if(i1.type == 1){
+                curCount ++;
+            }else{
+                curCount --;
+            }
+            //System.out.println(i1.getTime()+" "+i2.getTime()+" "+curCount);
+            if(curCount > 0){
+                totalTime += i2.getTime() - i1.getTime();
+            }
+        }
+        //int start = inters.get(0).start;
+        //int end = inters.get(inters.size() - 1).end;
+        //System.out.println(totalTime+","+lookup[0][0].aloneTime);
+        pw.println(totalTime );
+        pw.close();
+        //System.out.println(inters);
     }
 }
-class Interval{
+
+class Interval implements Comparable<Interval> {
     int start, end;
     int type = 0;
     int aloneTime = 0;
     int id;
-    public Interval(int start, int end, int type, int id){
+
+    public Interval(int start, int end, int type, int id) {
         this.start = start;
         this.end = end;
         this.type = type;
@@ -73,5 +119,15 @@ class Interval{
     public String toString() {
         return "Interval [aloneTime=" + aloneTime + ", end=" + end + ", start=" + start + ", type=" + type + "]";
     }
-    
+
+    public int getTime() {
+        return (this.type == 1) ? this.start : this.end;
+    }
+
+    @Override
+    public int compareTo(Interval o) {
+        // TODO Auto-generated method stub
+        return Integer.compare(this.getTime(), o.getTime());
+    }
+
 }
