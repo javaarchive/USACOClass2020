9ee69ba35c1e45bad32ff255ecff805259386c05
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-04 17:36:11 -0700

    one mine per square condition added

--- USACOClassLiveTest/src/minesweeper.java
+++ USACOClassLiveTest/src/minesweeper.java
@@ -42,28 +42,36 @@ public class minesweeper {
         return match;
     }
     static int Ktotal = 0;
+    static long sTime = System.currentTimeMillis();
+    static Set<Location> path2 = new HashSet<>();
     public static boolean recur(int K, int curMatch) {
         //System.out.println("recur("+K+","+curMatch+")");
         //System.out.println(K);
+        //System.out.print("\rElapsed Time in ms: "+(System.currentTimeMillis() - sTime)+" "+K);
         //progressPercentage(K, Ktotal);
+        if(K == 0){
+            // return true;
+            return false; 
+         }
+         if(K * 9 < (targetMatched - curMatch)){
+            return false; // Give up
+         }
         if(curMatch == targetMatched){
             return true;
-        }
-        if(curMatch > targetMatched){
+        }else if(curMatch > targetMatched){
             return false; // No point adding more mines
-        }
-        if(K == 0){
-           // return true;
-           return false; 
         }
         for(int i = 0; i < attempt.length; i ++){
             for(int j = 0; j < attempt[i].length; j ++){
-                if(valid(i,j)){
+                Location l = new Location(i,j);
+                if(valid(i,j) && !path2.contains(l)){
                     int newMatch = place(i,j, 1);
-                    path.add(new Location(i,j));
-                    if(recur(K - 1, curMatch = newMatch)){
+                    path.add(l);
+                    path2.add(l);
+                    if(recur(K - 1, curMatch + newMatch)){
                         return true;
                     }
+                    path2.remove(l);
                     path.remove(path.size() - 1);
                     place(i,j, -1); // Undo Action
                 }
@@ -132,6 +140,29 @@ class Location implements Comparable<Location>{
     public int compareTo(Location o) {
         // TODO Auto-generated method stub
         return (this.toString()).compareTo(o.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Location))
+            return false;
+        Location other = (Location) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
     }
     
 }
