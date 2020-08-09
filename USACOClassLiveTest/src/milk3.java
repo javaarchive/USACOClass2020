83bb3c25427ef03660e88c1e0a05798eb653942e
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-08 21:15:04 -0700

    fix!

--- USACOClassLiveTest/src/milk3.java
+++ USACOClassLiveTest/src/milk3.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class milk3 {
     public static int limit(int initial, int newAmount, int overflow) {
+        System.out.println("limit("+initial+","+newAmount+","+overflow+")");
         if ((initial + newAmount) > overflow) {
             return overflow - initial;
         }
-        return initial;
+        return newAmount;
     }
 
     public static void main(String[] args) throws IOException {
@@ -23,21 +24,66 @@ public class milk3 {
         boolean[][][] checked = new boolean[A + 1][B + 1][C + 1];
         Queue<State> states = new LinkedList<>();
         states.add(new State(0, 0, C));
+        TreeSet<Integer> answers = new TreeSet<>();
         while (!states.isEmpty()) {
             State s = states.poll();
-            if (s.A == 0) {
+            System.out.println("("+s.A+","+s.B+","+s.C+")");
+            if(s.A == 0){
+                System.out.println("Added "+s.C);
+                answers.add(s.C);
+            }
+            if (s.A != A) {
                 int pourAmount = limit(s.A, s.C, A);
                 int leftOver = s.C - pourAmount;
                 if (pourAmount != 0) {
                     State newState = new State(pourAmount + s.A, s.B, s.C - pourAmount);
-                    if(!checked[newState.A][newState.B][newState.C]){
+                    if (!checked[newState.A][newState.B][newState.C]) {
                         System.out.println("Pouring C to A, A would have " + (pourAmount + s.A));
                         checked[newState.A][newState.B][newState.C] = true;
                         states.add(newState);
                     }
-                    
+
                 }
             }
+            if (s.C != C) {
+                int pourAmount = limit(s.C, s.A, C);
+                int leftOver = s.A - pourAmount;
+                if (pourAmount != 0) {
+                    State newState = new State(s.A - pourAmount, s.B, s.C + pourAmount);
+                    if (!checked[newState.A][newState.B][newState.C]) {
+                        System.out.println("Pouring A to C, C would have " + (pourAmount + s.C));
+                        checked[newState.A][newState.B][newState.C] = true;
+                        states.add(newState);
+                    }
+                }
+            }
+            if (s.B != B) {
+                int pourAmount = limit(s.B, s.A, B);
+                int leftOver = s.A - pourAmount;
+                if (pourAmount != 0) {
+                    State newState = new State(s.A - pourAmount, s.B + pourAmount, s.C);
+                    if (!checked[newState.A][newState.B][newState.C]) {
+                        System.out.println("Pouring A to B, B would have " + (pourAmount + s.B));
+                        checked[newState.A][newState.B][newState.C] = true;
+                        states.add(newState);
+                    }
+                }
+            }
+            if (s.B != B) {
+                int pourAmount = limit(s.B, s.C, B);
+                int leftOver = s.C - pourAmount;
+                if (pourAmount != 0) {
+                    State newState = new State(s.A , s.B + pourAmount, s.C - pourAmount);
+                    if (!checked[newState.A][newState.B][newState.C]) {
+                        System.out.println("Pouring C to B, B would have " + (pourAmount + s.B));
+                        checked[newState.A][newState.B][newState.C] = true;
+                        states.add(newState);
+                    }
+                }
+            }
+        }
+        for(int ans: answers){
+            System.out.print(ans+" ");
         }
     }
 }
