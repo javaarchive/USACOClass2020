b1c27170ec32133886bf896e875ea137c9c16504
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-10 11:50:13 -0700

    remove edges code finished

--- USACOClassLiveTest/src/flow.java
+++ USACOClassLiveTest/src/flow.java
@@ -10,8 +10,8 @@ public class flow {
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(f.readLine());
         int nextId = 0;
-        List<List<Integer>> graph = new ArrayList<>(N);
-        List<List<Integer>> rgraph = new ArrayList<>(N);
+        List<List<Integer>> graph = new ArrayList<>(2*N); // worst case
+        List<List<Integer>> rgraph = new ArrayList<>(2*N); // worst case
         Map<String, Integer> stringtoid = new TreeMap<>();
         for(int i = 0; i < N; i ++){
             graph.add(new ArrayList<>());
@@ -35,13 +35,46 @@ public class flow {
         }
         int start = stringtoid.get("A");
         int end = stringtoid.get("Z");
-        boolean[] visited = new boolean[N];
+        boolean[] visited = new boolean[graph.size()];
         // Find edges to delete
         visited[end] = true;
+        Queue<MovementChoice> searchQ = new LinkedList<>();
+        while(!searchQ.isEmpty()){
+            MovementChoice mc = searchQ.poll();
+            for(int node: graph.get(mc.node)){
+                if(!visited[node]){
+                    visited[node] = true;
+                    searchQ.add(new MovementChoice(node));
+                }
+            }
+            for(int node: rgraph.get(mc.node)){
+                if(!visited[node]){
+                    visited[node] = true;
+                    searchQ.add(new MovementChoice(node));
+                }
+            }
+        }
+        for(int i = 0; i < graph.size(); i ++){
+            for(int otherNode: graph.get(i)){
+                if(!visited[otherNode] && !visited[i]){
+                    graph.get(i).remove((Object) otherNode);
+                    rgraph.get(otherNode).remove((Object) i);
+                }
+            }
+        }
+        for(int i = 0; i < rgraph.size(); i ++){
+            for(int otherNode: rgraph.get(i)){
+                if(!visited[otherNode] && !visited[i]){
+                    rgraph.get(i).remove((Object) otherNode);
+                    graph.get(otherNode).remove((Object) i);
+                }
+            }
+        }
     }
 }
 class MovementChoice{
+    int node;
     public MovementChoice(int node){
-
+        this.node = node;
     }
 }
