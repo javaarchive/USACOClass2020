332b3c92ee2f0afcab832118cf681f9bad6efa24
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-17 12:21:10 -0700

    ???????

--- USACOClassLiveTest/src/cowyotz.java
+++ USACOClassLiveTest/src/cowyotz.java
@@ -9,36 +9,45 @@ public class cowyotz {
 
     public static void recur(int side, int depth) {
         if (depth > N) {
-            boolean failed = false;
-            // System.out.println(Arrays.toString(freq));
-            for (int i = 0; i < N; i++) {
-                if (freq[i] < desired[i]) {
-                    failed = true;
+            boolean working = false;
+            //System.out.println(Arrays.toString(freq));
+            for (int j = 0; j < E; j++) {
+                boolean failed = false;
+                for (int i = 0; i < N; i++) {
+                    if (freq[i] < desired[j][i]) {
+                        failed = true;
+                        break;
+                    }
+                }
+                if (!failed) {
+                    working = true;
                     break;
                 }
             }
-            if (!failed) {
-                ans++;
+            if(working){
+                ans ++;
             }
             return;
         }
         int leftToPlace = N - depth + 1;
         int nonworking = 0;
         freq[side]++;
+        
         for (int j = 0; j < E; j++) {
             boolean nw = true;
             for (int i = 0; i < S; i++) {
 
-                if (desired[i] - freq[j][i] > leftToPlace) {
-                    // System.out.println("Prune "+desired[i] + " "+ freq[i]+" "+leftToPlace);
+                if (desired[j][i] - freq[i] > leftToPlace) {
+                    //System.out.println("Prune "+desired[i] + " "+ freq[i]+" "+leftToPlace);
                     nw = true;
                     break;
                 }
             }
-            if(nw){
-                nonworking ++;
+            if (nw) {
+                nonworking++;
             }
         }
+        System.out.println(nonworking);
         if (nonworking != E) {
             for (int i = 0; i < S; i++) {
                 recur(i, depth + 1);
@@ -46,7 +55,6 @@ public class cowyotz {
         }
         freq[side]--;
     }
-
     public static void main(String[] args) throws IOException {
         // new FileReader("pathfindin")
         BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
@@ -61,22 +69,17 @@ public class cowyotz {
         E = Integer.parseInt(st.nextToken()); // Expressions
         freq = new int[S]; // Side count
         desired = new int[E][S];
-        Arrays.fill(desired, Integer.MAX_VALUE);
+        //Arrays.fill(desired, Integer.MAX_VALUE);
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(f.readLine(), "+");
             while (st.hasMoreTokens()) {
                 String[] tokens = st.nextToken().split("x");
                 int count = Integer.parseInt(tokens[0]);
                 int side = Integer.parseInt(tokens[1]) - 1;
-                desired[side] = Integer.min(desired[side], count);
+                desired[i][side] = count;
             }
         }
-        for (int i = 0; i < desired.length; i++) {
-            if (desired[i] == Integer.MAX_VALUE) {
-                desired[i] = 0;
-            }
-        }
-        System.out.println(Arrays.toString(desired));
+        System.out.println(Arrays.deepToString(desired));
         for (int i = 0; i < S; i++) {
             recur(i, 1);
         }
