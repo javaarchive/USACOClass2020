99853f15e75a611fb8d72a9fab1b01eef894b904
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-23 21:10:12 -0700

    rearrange cereal

--- USACO2020Grind/src/cereal.java
+++ USACO2020Grind/src/cereal.java
@@ -1,56 +1,77 @@
-import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class cereal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // IO
-        BufferedReader f = new BufferedReader(new FileReader("cownomics.in"));
+        BufferedReader f = new BufferedReader(new FileReader("cereal.in"));
         // BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cownomics.out")));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cereal.out")));
         // PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(f.readLine());
-        int N = Integer.parseInt(f.readLine());
-        int[] cerealTakeId = new int[M];
-        Arrays.fill(a, val);
-        List<CerealCow> cows = new ArrayList<>(M);
-        for(int i = 0; i < M; i ++){
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] cerealTakeId = new int[M + 1];
+        // Arrays.fill(a, val);
+        List<CerealCow> cows = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(f.readLine());
-            int a,b;
+            int a, b;
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
-            CerealCow cc = new CerealCow(a,b);
+            CerealCow cc = new CerealCow(a, b);
             cows.add(cc);
         }
-        int satsified = false;
-        ArrayList<CerealCow> answers = new List<>();
-        //Arrays.fill(cre, val);
+        // int satsified = 0;
+        List<Integer> answers = new ArrayList<>();
+        // Arrays.fill(cre, val);
         int happyCows = 0;
-        for(int i  = N-1; i >= 0; i --){
+        for (int i = N - 1; i >= 0; i--) {
+            System.out.println("Add "+i);
             CerealCow cc = cows.get(i);
-            happyCows ++;
+            happyCows++;
             cc.taken = 1;
-            if(cerealTakeId[cc.first] > 0){ // If a cow is occupying that cereal kick it off
-                int id = cerealTakeId[i] - 1;
-                CerealCow rcow = cows.get(id);
-                if(rcow.taken == 2){ // Poor cow's second choice gets no cereal
-                    happyCows ++;
-                }else{
-                    if(cerealTakeId[rcow.second] > 0){ // Kick another cow off
-
+            int kickCow = cerealTakeId[cc.first] - 1;
+            happyCows++;
+            cerealTakeId[cc.first] = i + 1;
+            cc.taken = 1;
+            if (kickCow > -1) {
+                while (true) {
+                    System.out.println(kickCow + " Kicked "+Arrays.toString(cerealTakeId) );
+                    CerealCow nextCow = cows.get(kickCow);
+                    int cowToWrite = kickCow;
+                    if (cerealTakeId[nextCow.second] == 0 && nextCow.taken == 1) {
+                        cerealTakeId[nextCow.second] = kickCow + 1;
+                        break;
+                    }
+                    if (nextCow.taken == 1) {
+                        nextCow.taken = 2; // Switched to second cow
+                        kickCow = cerealTakeId[nextCow.second] - 1;
+                        cerealTakeId[nextCow.second] = cowToWrite + 1;
+                    } else if (nextCow.taken == 2) {
+                        happyCows--;
+                        nextCow.taken = 0;
+                        //cerealTakeId[nextCo]
+                        break; // Yay! End of the kicking chain
+                    }
+                    if(cowToWrite == kickCow){
+                        System.out.println("Self kick "+kickCow);
+                        //break; // No self kicking
                     }
                 }
             }
+            answers.add(happyCows);
         }
         Collections.reverse(answers);
+        System.out.println(happyCows+" "+answers);
     }
 }
-class CerealCow{
+
+class CerealCow {
     int first, second;
     int taken = 0;
-    public CerealCow(int first, int second){
+
+    public CerealCow(int first, int second) {
         this.first = first;
         this.second = second;
     }
