95e37d54c0584948007bdefbb67d7844d1fa6434
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-01 22:08:48 -0700

    works

--- USACO2020Grind/src/paintbarn.java
+++ USACO2020Grind/src/paintbarn.java
@@ -12,89 +12,54 @@ public class paintbarn {
         // BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("paintbarn.out")));
         // PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-        Map<Integer, List<Endpoint>> mapy = new TreeMap<>();
+        //Map<Integer, List<Endpoint>> mapy = new TreeMap<>();
         StringTokenizer st = new StringTokenizer(f.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
         // 4 3
         // 1 2
+        int[][] ps = new int[1002][1002];
         for(int i = 0; i < N; i ++){
             st = new StringTokenizer(f.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            Endpoint e1 = new Endpoint(x1,y1,x2,y2,1);
-            Endpoint e2 = new Endpoint(x1,y1,x2,y2,2);
-            Endpoint e3 = new Endpoint(x1,y1,x2,y2,3);
-            Endpoint e4 = new Endpoint(x1,y1,x2,y2,4);
-            if(!mapy.keySet().contains(y1)){
-                mapy.put(y1, new ArrayList<>());
-            }
-            if(!mapy.keySet().contains(y2)){
-                mapy.put(y2, new ArrayList<>());
-            }
-            mapy.get(y1).add(e1);
-            mapy.get(y1).add(e2);
-            mapy.get(y2).add(e3);
-            mapy.get(y2).add(e4);
+            ps[x1][y1] ++;
+            ps[x1][y2] --;
+            ps[x2][y1] --;
+            ps[x2][y2] ++;
         }
-        Comparator<Endpoint> c = new Comparator<Endpoint>(){
-
-            @Override
-            public int compare(Endpoint o1, Endpoint o2) {
-                // TODO Auto-generated method stub
-                return Integer.compare(o1.getX(), o2.getX());
+        int ans = 0;
+        /*for(int i = 0; i < 13; i ++){
+            for(int j = 0; j < 13; j ++){
+                System.out.print(ps[i][j]+" ");
             }
-        };
-        for(int y: mapy.keySet()){
-            mapy.get(y).sort(c);
-        }
-        Integer[] keys = (Integer[]) mapy.keySet().toArray();
-        for(int i = 0; i < keys.length-1; i ++){
-            int yVal1 = keys[i];
-            int yVal2 = keys[i + 1];
-            int dist = yVal2 - yVal1;
-            List<Endpoint> endpoints = mapy.get(yVal1).stream().filter(e -> (e.type == 1 || e.type == 2)).collect(Collectors.toList());
-            endpoints.addAll(mapy.get(yVal2).stream().filter(e -> (e.type == 3 || e.type == 4)).collect(Collectors.toList()));
-            endpoints.sort(c);
-            int covered = 0;
-            for(Endpoint e: endpoints){
-                if(e.type == 1){
-                    if(yVal1 <= e.y1 && e.y1 <= yVal2){
-                        covered ++;
-                    }
+            System.out.println();
+        }*/
+        for(int i = 0; i < 1001; i ++){
+            for(int j = 0; j < 1001; j ++){
+                int temp = ps[i][j];
+                if(i > 0){
+                    temp += ps[i - 1][j];
                 }
-
-                if(e.type == 2){
-                    if(yVal1 <= e.y1 && e.y1 <= yVal2){
-                        covered ++;
-                    }
+                if(j > 0){
+                    temp += ps[i][j - 1];
                 }
+                if(i > 0 && j > 0){
+                    temp -= ps[i-1][j-1];
+                }
+                /*if(temp > 0){
+                    System.out.println(temp);
+                }*/
+                    if(temp == K){
+                    ans ++;
+                }
+                ps[i][j] = temp;
             }
         }
-    }
-}
-class Endpoint{
-    int x1, y1, x2, y2, type;
-    public Endpoint(int x1, int y1, int x2, int y2, int type){
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.type = type;
-    }
-    public int getX(){
-        switch(this.type){
-            case 2:
-            case 3:
-                return this.x2;
-            case 1:
-            case 4:
-                return this.x1;
-            default:
-                return -1;
-        }
+        pw.println(ans);
+        pw.close();
     }
 }
