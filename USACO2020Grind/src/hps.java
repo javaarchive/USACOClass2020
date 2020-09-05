d11fae3ff61b279bb658cd17bdc9a5df60fa1590
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-04 19:34:09 -0700

    before revert

---
+++ USACO2020Grind/src/hps.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+public class hps {
+    public static int best(int[] arr){
+        //System.out.println("Finding best of "+Arrays.toString(arr));
+        return Integer.max(arr[0], Integer.max(arr[1],arr[2]));
+    }
+    public static int[] sub(int[] top, int[] bottom){
+        //System.out.println(Arrays.toString(top));
+        //System.out.println(Arrays.toString(bottom));
+        int[] out = new int[3];
+        out[0] = top[0] - bottom[0];
+        out[1] = top[1] - bottom[1];
+        out[2] = top[2] - bottom[2];
+        return out;
+    }
+    public static void main(String[] args) throws IOException{
+         // IO
+         String inputfile = "hps.in";
+         if (args.length > 0) {
+             inputfile = args[0];
+         }
+         BufferedReader f = new BufferedReader(new FileReader(inputfile));
+         // BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("hps.out")));
+         StringTokenizer st = new StringTokenizer(f.readLine());
+         int N = Integer.parseInt(st.nextToken());
+         int K = Integer.parseInt(st.nextToken());
+         int[][] ps = new int[N + 1][3];
+         int h = 0, p = 0, s = 0;
+         for(int i = 0; i < N; i ++){
+            char type = f.readLine().charAt(0);
+            switch(type){
+                case 'H':
+                    h ++;
+                    break;
+                case 'P':
+                    p ++;
+                    break;
+                case 'S':
+                    s ++;
+                    break;
+            }
+            ps[i + 1][0] = h;
+            ps[i + 1][1] = p;
+            ps[i + 1][2] = s;
+         }
+         Set<Integer> divPlaces = new TreeSet<>();
+         int lower = 0;
+         int upper = N;
+         Integer[] dividers = new Integer[K];
+         //System.out.println(Arrays.deepToString(ps));
+         divPlaces.add(N - 1);
+         int bestSum = 0;
+         for(int i = 0; i < K; i ++){
+             System.out.println("======= K: "+i+" =======");
+            // Check Divider best
+            dividers = new Integer[divPlaces.size()];
+            divPlaces.toArray(dividers);
+            if(divPlaces.size() > 0){
+                upper = dividers[0];
+            }
+            int upplace = 0;
+            int bestPos = -1;
+            bestSum = 0;
+            for(int j = 0; j < N; j ++){
+                if(upper < j){
+                    lower = upper;
+                    upper = dividers[++upplace];
+                }
+                //System.out.println(lower+" "+upper);
+                //System.out.println("Right part " + best(sub(ps[upper], ps[j])));
+                //System.out.println("Left part " + best(sub(ps[j], ps[lower])));
+                int sum = best(sub(ps[upper + 1], ps[j])) + best(sub(ps[j + 1], ps[lower])) + best(sub(ps[N],ps[upper])) +  best(sub(ps[lower + 1], ps[0]));
+                //System.out.println("Sum "+sum);
+                if(sum > bestSum){
+                    if(divPlaces.contains(j)){
+                        continue;
+                    }
+                    bestPos = j;
+                    bestSum = sum;
+                }
+            }
+            divPlaces.add(bestPos);
+         }
+         pw.println(bestSum);
+         pw.close();
+    }
+}
