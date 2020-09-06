1a81dfaa92e0404940675dc8022adb5036ba8578
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-06 13:36:00 -0700

    overshoot

--- USACO2020Grind/src/checklist_slow.java
+++ USACO2020Grind/src/checklist_slow.java
@@ -3,20 +3,26 @@ import java.util.*;
 
 public class checklist {
     static int[][] dp;
-    static int[][] dpIds;
     static int[][] Hcows, Gcows;
     static int dist(int[] a, int[] b){
         int A = a[0] - b[0];
         int B = a[1] - b[1];
         return A * A + B * B;
     }
-    static int[] getPoint(int id){
-        if(id > 0){
-            return Hcows[id - 1];
-        }else if(id < 0){
-            return Gcows[-id - 1];
+    static void recur(int Hpos,int Gpos, int[] point,boolean useH, int currentDist){
+        currentDist += dist(point, (useH?Hcows:Gcows)[useH?Hpos:Gpos]);
+        System.out.println(Hpos + " " + Gpos+" : "+currentDist);
+        if(dp[Hpos][Gpos] > currentDist){
+            dp[Hpos][Gpos] = currentDist;
+            if(Hpos < Hcows.length-1 && Gpos != 0){
+                recur(Hpos + 1, Gpos, (useH?Hcows:Gcows)[useH?Hpos:Gpos], true, currentDist);
+            }
+            if(Gpos < Gcows.length-1 && Hpos != 0){
+                recur(Hpos, Gpos + 1, (useH?Hcows:Gcows)[useH?Hpos:Gpos], false, currentDist);
+            }
+        }else{
+            return; // La prune
         }
-        return new int[2];
     }
     public static void main(String[] args) throws IOException{
         String inputfile = "checklist.in";
@@ -41,35 +47,13 @@ public class checklist {
             Gcows[i][0] = Integer.parseInt(st.nextToken());
             Gcows[i][1] = Integer.parseInt(st.nextToken());
         }
-        dp = new int[H + 1][G + 1]; // 0 = now cows iterated yet
-        dpIds = new int[H + 1][G + 1];
+        dp = new int[H][G];
         for(int i = 0; i < dp.length; i ++){
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        dp[0][0] = 0;
-        dpIds[0][0] = 1;
-        for(int i = 0; i < H; i ++){
-            dp[i + 1][0] = dist(Hcows[0], Hcows[i]);
-            dpIds[i + 1][0] = i + 1;
-        }
-        for(int i = 0; i < G; i ++){
-            dp[0][i + 1] = dist(Gcows[0], Gcows[i]);
-            dpIds[0][i + 1] = -(i + 1);
-        }
-        for(int i = 1; i <= H; i ++){
-            for(int j = 1; j <= G; j ++){
-                int opt1 = dist(getPoint(dpIds[i - 1][j]),Hcows[i - 1]) + dp[i - 1][j];
-                int opt2 = dist(getPoint(dpIds[i][j - 1]),Gcows[j - 1]) + dp[j][i - 1];
-                if(opt1 > opt2){
-                    dp[i][j] = opt1;
-                    dpIds[i][j] = i - 1;
-                }else{
-                    dp[i][j] = opt2;
-                    dpIds[i][j] = j - 1;
-                }
-            }
-        }
-        pw.println(dp[H][G]);
+        recur(1, 0, Hcows[0], true, 0);
+        recur(0, 1, Hcows[0], false, 0);
+        pw.println(dp[H - 1][G - 1]);
         pw.close();
     }
 }
