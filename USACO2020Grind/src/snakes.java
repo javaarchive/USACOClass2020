08c6b1569aa14fb8fa55f1ece2ee4b0a464405ef
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-11 21:11:25 -0700

    ba

--- USACO2020Grind/src/snakes.java
+++ USACO2020Grind/src/snakes.java
@@ -35,7 +35,20 @@ public class snakes {
         for(int i = 0; i < N; i ++){
             System.out.println(Arrays.toString(dp[i]));
         }
+        int best = Integer.MAX_VALUE;
+        int[][] dp2 = new int[N][N];
+        for(int i = 0; i < N; i ++){
+            for(int j = 0; j < N; j ++){
+                dp2[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        dp2[0] = Arrays.copyOf(dp[0], dp.length);
+        //System.out.println("Row 1: "+Arrays.toString(dp2[0]));
         
+        for(int i = 0; i < N; i ++){
+            System.out.println(Arrays.toString(dp2[i]));
+        }
+        System.out.println(best);
 
         //pw.println(dp[N - 1]);
         pw.close();
