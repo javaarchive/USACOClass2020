9605735d49e908a4a7be9e1d185bd97699af4b8b
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-13 22:16:28 -0700

    works after IO clarifacation

--- USACOClassGold/src/msquare.java
+++ USACOClassGold/src/msquare.java
@@ -9,48 +9,59 @@ public class msquare {
         // new BufferedWriter(new FileWriter("msquare.out"))
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(f.readLine());
-        int[] firstState = new int[8];
-        for (int i = 0; i < 8; i++) {
-            firstState[i] = Integer.parseInt(st.nextToken());
+        int[] endState = new int[8];
+        for (int i = 0; i < 4; i++) {
+            endState[i] = Integer.parseInt(st.nextToken());
         }
+        for (int i = 7; i >= 4; i--) {
+            endState[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] firstState = {1,2,3,4,8,7,6,5};
         Queue<PlanarSquareState> q = new LinkedList<PlanarSquareState>();
         PlanarSquareState pss = new PlanarSquareState(firstState);
         Map<Integer, String> visited = new TreeMap<>();
         q.add(pss);
         while(!q.isEmpty()){
             PlanarSquareState curPSS = q.poll();
+            if(Arrays.equals(curPSS.nums, endState)){
+                pw.println(curPSS.s.length());
+                pw.println(curPSS.s);
+                break;
+            }
+            //System.out.println(Arrays.toString(curPSS.nums)+" "+curPSS.s);
             PlanarSquareState a = new PlanarSquareState(curPSS.transformA());
             PlanarSquareState b = new PlanarSquareState(curPSS.transformB());
             PlanarSquareState c = new PlanarSquareState(curPSS.transformC());
-            a.s += "A";
-            b.s += "B";
-            c.s += "C";
+            a.s = curPSS.s + "A";
+            b.s = curPSS.s + "B";
+            c.s = curPSS.s + "C";
             int ha = a.hashCode(), hb = b.hashCode(), hc = c.hashCode();
            // System.out.println(ha);
             if(!visited.keySet().contains(ha)){
-                if(a.s.compareTo(visited.get(ha)) < 0){
-                    visited.put(ha, a.s);
-                }
+                visited.put(ha, a.s);
+                q.add(a);
             }else if(a.s.compareTo(visited.get(ha)) < 0){
                 visited.put(ha, a.s);
+                q.add(a);
             }
             if(!visited.keySet().contains(hb)){
-                if(b.s.compareTo(visited.get(hb)) < 0){
-                    visited.put(hb, b.s);
-                }
+                visited.put(hb, b.s);
+                q.add(b);
             }else if(b.s.compareTo(visited.get(hb)) < 0){
                 visited.put(hb, b.s);
+                q.add(b);
             }
             if(!visited.keySet().contains(hc)){
-                if(c.s.compareTo(visited.get(hc)) < 0){
-                    visited.put(hc, c.s);
-                }
+                visited.put(hc, c.s);
+                q.add(c);
             }else if(c.s.compareTo(visited.get(hc)) < 0){
+                q.add(c);
                 visited.put(hc, c.s);
             }
 
         }
-
+        pw.close();
+        f.close();
     }
 }
 
@@ -65,10 +76,10 @@ class PlanarSquareState implements Comparable<PlanarSquareState> {
     public int[] transformA() {
         int[] in = this.nums;
         int[] out = new int[8];
-        out[0] = in[7];
-        out[1] = in[6];
-        out[2] = in[5];
-        out[3] = in[4];
+        out[0] = in[4];
+        out[1] = in[5];
+        out[2] = in[6];
+        out[3] = in[7];
         out[4] = in[0];
         out[5] = in[1];
         out[6] = in[2];
@@ -79,14 +90,14 @@ class PlanarSquareState implements Comparable<PlanarSquareState> {
     public int[] transformB() {
         int[] in = this.nums;
         int[] out = new int[8];
-        out[0] = in[1];
-        out[1] = in[2];
-        out[2] = in[3];
-        out[3] = in[0];
-        out[4] = in[5];
-        out[5] = in[6];
-        out[6] = in[7];
-        out[7] = in[4];
+        out[0] = in[3];
+        out[1] = in[0];
+        out[2] = in[1];
+        out[3] = in[2];
+        out[4] = in[7];
+        out[5] = in[4];
+        out[6] = in[5];
+        out[7] = in[6];
         return out;
     }
 
@@ -99,7 +110,7 @@ class PlanarSquareState implements Comparable<PlanarSquareState> {
         out[3] = in[3];
         out[4] = in[4];
         out[5] = in[6];
-        out[6] = in[3];
+        out[6] = in[2];
         out[7] = in[7];
         return out;
     }
