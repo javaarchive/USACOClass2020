d69df31416c04fe5c24c1df79fc09e3661e18068
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-16 21:58:41 -0700

    ahhhhh

--- USACOClassGold/src/scales.java
+++ USACOClassGold/src/scales.java
@@ -2,24 +2,44 @@ import java.io.*;
 import java.util.*;
 public class scales {
     static int[] ps;
+    static int[] arr;
     static int C;
-    public static int recur(int[] bps, int i){
-        if(i <= 0){
-            return bps[0];
+    public static int recur(int curSum, int i){
+        System.out.println("RECUR("+curSum+","+i+");");
+        if(i < 0){
+            if(curSum <= C){
+                return curSum;
+            }else{
+                return -1;
+            }
         }
-        int a = bps[i - 1];
-        int b = bps[i];
+
+        int a = 0;
+        if(i > 0){
+            a = arr[i - 1];
+        }
+        int b = arr[i];
+        System.out.println(a + " " + b);
         //int c = ps[i];
-        if(a + b <= C){
-            bps[i - 2] = b;
-            return recur(bps, i - 2);
+        if(i == 0){
+            if((curSum + arr[0]) > C){
+                return -1;
+            }else{
+                //System.out.println("R!");
+                return curSum + arr[0];
+            }
+        }else if(a + b <= C){
+            int r = Integer.max(recur(curSum + b, i - 2),recur(curSum + b + a, i - 2));
+            System.out.println("recur("+(i-2)+") = "+r);
+            return r;
         }else if(b > C){
-            bps[i - 2] = a;
-            return recur(bps, i - 2);
+            int r = Integer.max(recur(curSum + a, i - 2),recur(curSum, i - 2));
+            System.out.println("recur("+(i - 2)+") = "+r);
+            return r;
         }else if(a + b > C){
-            return Integer.max(recur(bps, i + 1),recur(bps, i));
+            return Integer.max(recur(curSum + a, i - 2),recur(curSum + b, i - 2));
         }else{
-            return bps[i];
+            return 0;
         }
     }
     public static void main(String[] args) throws IOException{
@@ -31,14 +51,13 @@ public class scales {
         StringTokenizer st = new StringTokenizer(f.readLine());
         int N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int[] bps = new int[N];
+        arr = new int[N];
+        //int[] bps = new int[N];
         ps = new int[N + 1];
-        bps[N - 1] = arr[N - 1];
+        //bps[N - 1] = arr[N - 1];
         for(int i = 0; i < N; i ++){
             arr[i] = Integer.parseInt(f.readLine());
-            ps[i + 1] = ps[i] + arr[i];
         }
-        System.out.println(recur(bps, N - 1));
+        System.out.println(recur(0, N - 1));
     }
 }
