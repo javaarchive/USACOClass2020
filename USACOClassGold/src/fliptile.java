737dd3762d1eb55c4aeb9adae4362b58ac8f3ad0
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-09-24 21:21:09 -0700

    fixes

--- USACOClassGold/src/fliptile.java
+++ USACOClassGold/src/fliptile.java
@@ -8,7 +8,7 @@ public class fliptile {
         return (A != B) ? '1':'0';
     }
     public static char NOT(char c){
-        return (c == 'a') ? 'b':'a';
+        return (c == '1') ? '0':'1';
     }
     public static void main(String[] args) throws IOException{
         // IO
@@ -37,31 +37,96 @@ public class fliptile {
         }
         System.out.println(Arrays.toString(preTable));
         int TwoPowM = (int) Math.pow(2, M);
+        boolean hasSol = false;
         for(int i = 0 ;i < TwoPowM; i ++){
             System.out.println("Possible: "+i);
             int[] build = new int[N];
             build[0] = i;
-            String top = String.format("%"+M+"s",Integer.toBinaryString(build[j])).replace(' ','0');
-            System.out.println(String.format("%"+M+"s",Integer.toBinaryString(build[j])).replace(' ','0'));
+            String top = String.format("%"+M+"s",Integer.toBinaryString(build[0])).replace(' ','0');
+            //System.out.println(String.format("%"+M+"s",Integer.toBinaryString(build[0])).replace(' ','0'));
             char[][] grid = new char[N][M];
-            for(int j = 0; j < M; j ++){
-                grid[0][j] = top.charAt(j);
+            for(int j = 0; j< N; j ++){
+                for(int k = 0; k< M; k ++){
+                    grid[j][k] = target[j][k];
+                }
             }
+            int[][] ans = new int[N][M];
+           /* for(int j = 1; j < N; j ++){
+                Arrays.fill(grid[j], '0');
+            }*/
+            for(int j = 0; j < M; j ++){
+                //grid[0][j] = top.charAt(j);
+                if(top.charAt(j) == '1' ){
+                    
+                    if(j > 0){
+                        grid[0][j - 1] = NOT(grid[0][j - 1]);
+                    }
+                    grid[0][j] = NOT(grid[0][j]);
+                    if(j < M - 1){
+                        grid[0][j + 1] = NOT(grid[0][j + 1]);
+                    }
+                    grid[1][j] = NOT(grid[1][j]);
+                }
+                ans[0][j] = Integer.parseInt(Character.toString(top.charAt(j)));
+            }
+           
+            
             for(int j = 1; j < N; j ++){
                 if(j >= 2){
-                    if(!Arrays.equals(grid[j - 2], target[j - 2]){
+                    if(!Arrays.equals(grid[j - 2], target[j - 2])){
                         break;
                     }
                 }
-                for(int k = 0; k < N; k ++){
-                    
+                for(int k = 0; k < M; k ++){
+                    if(grid[j - 1][k] == 1){ // Must Flip
+                        ans[j][k] = 1;
+                        grid[j][k] = NOT(grid[j][k]);
+                        if(k > 0){	
+                            grid[j][k - 1] = NOT(grid[j][k - 1]);	
+                        }	
+                        if(k < M - 1){	
+                            grid[j][k + 1] = NOT(grid[j][k + 1]);	
+                        }	
+                        if(j > 0){	
+                            grid[j - 1][k] = NOT(grid[j - 1][k]);	
+                        }	
+                        if(j < N - 1){	
+                            grid[j + 1][k] = NOT(grid[j + 1][k]);	
+                        }
+                    }
                 }
             }
-            
+            boolean solCorrect = true;
+            for(int j = 0; j < N; j ++){
+                for(int k = 0; k < M; k ++){
+                    if(grid[j][k] == 1){
+                        solCorrect = false;
+                        break;
+                    }
+                }
+            }
+            if(solCorrect){
+                hasSol = true;
+                pw.println("Found a solution "+i);
+                for(int j = 0; j < N; j ++){
+                    //pw.print("sc: ");
+                    for(int k = 0; k < M; k ++){
+                        
+                        pw.print(ans[j][k]+" ");
+                    }
+                    pw.println();
+                }
+                //break;
+            }
             for(int j = 0 ; j < N; j ++){
-                    // System.out.println(String.format("%"+M+"s",Integer.toBinaryString(build[j])).replace(' ','0'));
+                    //System.out.println(String.format("%"+M+"s",Integer.toBinaryString(build[j])).replace(' ','0'));
                     System.out.println(Arrays.toString(grid[j]));
             }
+            
         }
+        if(!hasSol){
+            pw.println("IMPOSSIBLE");
+        }
+        pw.close();
     }
 }
