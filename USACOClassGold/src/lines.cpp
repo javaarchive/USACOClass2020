fde518e5ba86483dc09d1e111032ff079300d852
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-10-07 21:52:11 -0700

    BACKUP

--- USACOClassGold/src/lines.cpp
+++ USACOClassGold/src/lines.cpp
@@ -2,12 +2,33 @@
 #include <set>
 using namespace std;
 
+struct fraction{
+    int a,b;
+    bool equals(fraction f2){
+        return (this->a * f2.b) == (this->b * f2.a);
+    }
+    bool operator==(const fraction& f2) const{
+        return (this->a * f2.b) == (this->b * f2.a);
+    }
+    bool operator<(const fraction& f2) const{
+        return (this->a * f2.b) < (this->b * f2.a);
+    }
+};
 int N;
 const int MAXN = 201;
 int pointsX[MAXN];
 int pointsY[MAXN];
+set<fraction, greater<fraction> > slopes;
 int solve(){
-    for(int i = 0; i < N; i )
+    for(int i = 0; i < N; i ++){
+        for(int j = i + 1; j < N; j ++){
+            fraction f;
+            f.a = pointsX[i] - pointsX[j];
+            f.b = pointsY[i] - pointsY[j];
+            slopes.insert(f);
+        }
+    }
+    return slopes.size();
 }
 int main(int argc, const char** argv) {
     cin >> N;
