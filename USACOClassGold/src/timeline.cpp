07db5ca6f655bcd9d0708454a97d72dd978ace62
Raymond <javaarchive@users.noreply.github.com>
2020-11-25 19:38:12 -0800

    finish timeline

--- USACOClassGold/src/timeline.cpp
+++ USACOClassGold/src/timeline.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#define MAXSESSIONS 1001
+#define MAXSESSIONS 100001
 
 using namespace std;
 
@@ -10,6 +10,7 @@ int earliest[MAXSESSIONS] = {0};
 int inwards[MAXSESSIONS] = {0};
 int outwards[MAXSESSIONS] = {0};
 map<int, vector<pair<int, int>>> dGraph;
+map<int, vector<pair<int, int>>> rGraph;
 vector<int> topSort() {
     vector<int> indegreesStart;
     vector<int> newIndegrees;
@@ -22,9 +23,9 @@ vector<int> topSort() {
     int iter = 0;
     while (!indegreesStart.empty()) {
         //cout << indegreesStart.size() << endl;
-        for(int i = 0; i < N; i ++){
+        /*for(int i = 0; i < N; i ++){
             cout << i << " IN: " << inwards[i] << " and OUT:" << outwards[i] << endl;  
-        }
+        }*/
         for (auto iter = indegreesStart.begin(); iter != indegreesStart.end(); iter++) {
             int pos = *iter;
             //cout << pos<<":"<<dGraph[pos].size() << endl;
@@ -43,7 +44,7 @@ vector<int> topSort() {
         }
         indegreesStart = newIndegrees;
         newIndegrees = {}; // Reset vector
-        cout << iter << endl;
+        //cout << iter << endl;
     }
     return ans;
 }
@@ -53,20 +54,38 @@ int main(int argc, char const* argv[])
     /* code */
     cin >> N >> M >> C;
     for (int i = 0; i < N; i++) {
-        cin >> earliest[i];
+        cin >> earliest[i + 1];
     }
     for (int i = 0; i < C; i++) {
         int a, b, len;
         cin >> a >> b >> len;
         //                            days session
         dGraph[a].push_back(make_pair(len, b));
+        rGraph[b].push_back(make_pair(len, a));
         inwards[b] ++;
         outwards[a] ++;
     }
     vector<int> ans = topSort();
-    cout << "Result: " << endl;
+    /*cout << "Result: " << endl;
     for (int i = 0; i < ans.size(); i++) {
         cout << i << ": " << ans[i] << endl;
+    }*/
+    int finalizedTimes[MAXSESSIONS];
+    for(int i = 0; i < ans.size(); i ++){
+        // Traditional Iteration
+        int session = ans[i];
+        if(rGraph[session].empty()){
+            finalizedTimes[session] = earliest[session];
+        }else{
+            int maxExtra = earliest[session];
+            for(auto iter = rGraph[session].begin(); iter != rGraph[session].end(); iter ++){
+                maxExtra = max(maxExtra, finalizedTimes[iter->second]+iter->first);
+            }
+            finalizedTimes[session] = maxExtra;
+        }
+    }
+    for (int i = 1; i < ans.size(); i++) {
+        cout << finalizedTimes[i] << endl;
     }
     return 0;
 }
