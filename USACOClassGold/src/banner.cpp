6b269ea39e744c6824fbaa810487c2a7e921eb21
Raymond <javaarchive@repl.email>
2020-11-27 21:04:45 -0800

    workon banner + test github again

--- USACOClassGold/src/banner.cpp
+++ USACOClassGold/src/banner.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 int W,H,l1,l2;
-
+long long solve(){
+    long long ans;
+    
+    return ans;
+}
 int main(int argc, const char** argv) {
     cin >> W >> H;
     W ++;
     H ++;
     cin >> l1 >> l2;
+    cout << solve();
     return 0;
 }
