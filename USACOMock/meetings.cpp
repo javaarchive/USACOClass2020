598bb07d87c7583f501b90228cf1bdb3a9e95ed1
Raymond <javaarchive@repl.email>
2020-12-20 22:41:37 -0800

    status checkpoint

---
+++ USACOMock/meetings.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+int N,L;
+void setIO(string s) {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+bool willMeetAt(int T, int x1, int x2){
+    return ((x1 + 2*T) >= x2);
+}
+struct Cow{
+    bool movingLeft;
+    int pos;
+    int weight;
+};
+Cow cows[50001];
+Cow nCows[50001];
+int main(int argc, const char** argv) {
+    setIO("meetings");
+    cin >> N >> L;
+    int z = 0;
+    for(int i = 0; i < N; i ++){
+        int dir;
+        cin >> cows[i].weight >> cows[i].pos >> dir;
+        cows[i].movingLeft = (dir == -1);
+        if(cows[i].movingLeft){
+            z ++;
+        }
+        //completionTimes.insert(cows[i].movingLeft?cows[i].pos:L-cows[i].pos);
+    }
+    sort(cows,cows + N, [](Cow a, Cow b){
+        int na = a.movingLeft?a.pos:L-a.pos;
+        int nb = b.movingLeft?b.pos:L-b.pos;
+        return na < nb;
+    });
+    int leftCursor = 0;
+    for(int i = 0; i < N; i ++){
+        if(cows[i].movingLeft){
+            nCows[i].movingLeft = cows[i].movingLeft;
+            nCows[i].weight = cows[i].weight;
+            nCows[i].pos = cows[leftCursors].pos;
+            leftCursor ++;
+        }
+    }
+    return 0;
+}
--- USACOMock/moobuzz.cpp
+++ USACOMock/moobuzz.cpp
@@ -12,14 +12,12 @@ int myshinytable[] = {1,2,4,7,8,11,13,14,16,17,19,22,23,26,28,29};//31 32 34 37 
 //                    1 2 3 4 5 6  7  8  9  10 11 12 13 14 15 16    17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33
 int main(int argc, const char **argv)
 {
-    //setIO("moobuzz");
+    setIO("moobuzz");
     int len = sizeof(myshinytable)/sizeof(myshinytable[0]);
     int N;
     cin >> N;
     N --;
-    cout << ((myshinytable[(N)%8])) << " " << (((int)(N/8)) * 15) << endl;
-    cout << (myshinytable[(N)%8]) << " " << floor((N)/8) * 15 << endl;
-    cout << (myshinytable[(N)%8]) << " " << N/8 * 15;
+    cout << myshinytable[N%8] + N/8 * 15<< endl;
 
     return 0;
 }
