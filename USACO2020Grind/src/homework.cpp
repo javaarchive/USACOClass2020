1d48f683cb3103a0c59482a6b26b674eb567dd86
javaarchive <javaarchive@repl.email>
2021-04-02 22:06:14 -0700

    checkpoint

--- USACO2020Grind/src/homework.cpp
+++ USACO2020Grind/src/homework.cpp
@@ -1,44 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MAXN 10001
+#define MAXN 100001
 using namespace std;
 int N;
 int scores[MAXN];
 int lowest[MAXN];
-struct fraction{
-    int a;
-    int b;
-    fraction(int numerator, int denominator){
-        int common = __gcd(numerator, denominator);
-        a = numerator/common;
-        b = denominator/common;
-    }
-    bool operator < (const fraction &right)
-    {
-        if(right.b == b){
-            return a < right.a;
-        }
-        return ((a/b) < (right.a/right.b));
-    }
-    bool operator ==(const fraction &right){
-        return a == right.a && b == right.b;
-    }
-};
-vector<pair<fraction,int>> answers;
+vector<pair<pair<int,int>,int>> answers;
 void setIO(string s) {
     ios_base::sync_with_stdio(0); cin.tie(0);
     freopen((s + ".in").c_str(), "r", stdin);
     freopen((s + ".out").c_str(), "w", stdout);
 }
-bool dbleq(double a, double b, double epsilon = 0.01)
-{
-    return std::abs(a - b) < epsilon;
+bool compare(long long aNum, long long aDen, long long bNum, long long bDen){
+    return (aNum*bDen > bNum*aDen);
 }
-
+bool compareRev(long long aNum, long long aDen, long long bNum, long long bDen){
+    return (aNum*bDen < bNum*aDen);
+}
+bool compareEq(long long aNum, long long aDen, long long bNum, long long bDen){
+    return (aNum*bDen == bNum*aDen);
+}
 int main(int argc, const char** argv) {
     if(argc != 2){
-        setIO("homework");
+        setIO("homework.5");
     }
     cin >> N;
     int sum = 0;
@@ -51,24 +36,25 @@ int main(int argc, const char** argv) {
         curMin = min(curMin, scores[i]);
         lowest[i] = curMin;
     }
-
+    int bestNumerator = 0;
+    int bestDenominator = 1;
     for(int i = 1; i <= N - 2; i ++){
         sum -= scores[i - 1];
         // cout << "((" << sum << " - " << lowest[i] << ") / " << (N - i - 1) << endl;
         // 1000000
-        fraction avg((((double) (sum - lowest[i]) * 1000000.0L)),((double) (N - i - 1)));
         // cout << "Average  " << avg << " at removal of " << i << endl;
-        answers.push_back(make_pair(avg,i));
+        answers.push_back(make_pair(make_pair((sum - lowest[i]),(N - i - 1)),i));
     }
-    sort(answers.begin(), answers.end(),[](pair<fraction,int> a, pair<fraction,int> b){
-        if(a.first == b.first){
+    sort(answers.begin(), answers.end(),[](pair<pair<int,int>,int> a, pair<pair<int,int>,int> b){
+        if(compareEq(a.first.first, a.first.second, b.first.first, b.first.second)){
             return a.second < b.second;
         }
-        return b.first < a.first;
+        return compare(a.first.first, a.first.second, b.first.first, b.first.second);
     });
-    fraction bestVal = (*answers.begin()).first;
+    pair<int,int> bestVal = (*answers.begin()).first;
+    // cout << "Best avg is " << bestVal.first/bestVal.second << endl;
     for(auto iter = answers.begin(); iter != answers.end(); iter ++){
-        if(iter->first == bestVal){
+        if(compareEq(iter->first.first,iter->first.second,bestVal.first,bestVal.second)){
             // cout << iter->first << " " << 
             cout << iter->second << endl;
         }else{
