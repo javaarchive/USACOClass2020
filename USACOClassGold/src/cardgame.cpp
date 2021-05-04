abcd3a5db70144e1c38a5a70d759bbce3dfef78d
Raymond <javaarchive@repl.email>
2021-05-03 22:09:09 -0700

    checkpoint

--- USACOClassGold/src/cardgame.cpp
+++ USACOClassGold/src/cardgame.cpp
@@ -1,62 +1,85 @@
 #include <iostream>
 #include <algorithm>
+#include <set>
 using namespace std;
 int N;
 int cards[50001];
+int sortedCards[50001]; // elsie's sorted cars
 int bcards[50001];
 
 int main(int argc, const char** argv) {
     cin >> N;
     for(int i = 0; i < N; i ++){
         cin >> cards[i];
+        sortedCards[i] = cards[i];
     }
-    sort(cards, cards + N);
+    sort(sortedCards, sortedCards + N);
     int b_index = 0;
     int e_index = 0;
     for(int i = 1; i <= 2 * N; i ++){
-        if(i != cards[e_index]){
+        if(i != sortedCards[e_index]){
             bcards[b_index] = i;
             b_index ++;
         }else{
             e_index ++;
         }
     }
-    /*cout << "Bessie has the cards ";
+    cout << "Bessie has the cards ";
     for(int i = 0; i < N; i ++){
         cout << bcards[i] << " ";
     }
-    cout << endl;*/
-    int b_index2 = N/2 - 1;
+    cout << endl;
+    // sort(cards, cards + N/2);
+    reverse(cards, cards + N/2);
+    reverse(bcards,bcards + N);
+    reverse(bcards + N/2,bcards + N);
+    sort(cards + N/2, cards + N);
+    int b_index2 = 0;
     int wins = 0;
-    for(int i = 0; i < N/2; i ++){
-        bool signalLoopEnd = false;
-        while(bcards[b_index2] >= cards[i]){ // while not good enough to beat
-            b_index2 --;
-            if(b_index2 < 0){
-                signalLoopEnd = true;
-                break;
-            }
-        }
-        if(signalLoopEnd){
-            break;
-        }
-        wins ++;
+    for(int i = 0; i < N; i ++){
+        cout << bcards[i] << " ";
     }
-    b_index2 = N/2; // set to a bit above mid
-    for(int i = N/2; i < N; i ++){
+    cout << endl;
+    for(int i = 0; i < N/2 && b_index2 < N/2; i ++){
         bool signalLoopEnd = false;
         while(bcards[b_index2] <= cards[i]){ // while not good enough to beat
-            b_index2 --;
-            if(b_index2 >= 0){
+            
+            cout << b_index2 << " ..." << endl;
+            if(b_index2 >= N/2){
+                signalLoopEnd = true;
+                break;
+            }
+            b_index2 ++;
+        }
+        if(signalLoopEnd || b_index2 >= N/2){
+            break;
+        }
+        cout << "bcards[" << b_index2 << "] and cards[" << i << "] " << endl;
+        cout << bcards[b_index2] << " beats " << cards[i] << endl;
+        wins ++;
+        b_index2 ++;
+    }
+    cout << "Flip" << endl;
+    b_index2 = N/2; // set to a bit above mid
+    for(int i = N/2; i < N && b_index2 < N; i ++){
+        bool signalLoopEnd = false;
+        while(bcards[b_index2] >= cards[i]){ // while not good enough to beat
+            cout << b_index2 << " ..." << endl;
+            if(b_index2 >= N){
                 break;
                 signalLoopEnd = true;
             }
+            b_index2 ++;
+            
         }
-        if(signalLoopEnd){
+        if(signalLoopEnd || b_index2 >= 0){
             break;
         }
+        cout << "bcards[" << b_index2 << "] and cards[" << i << "] " << endl;
+        cout << bcards[b_index2] << " beats " << cards[i] << endl;
         wins ++;
+        b_index2 ++;
     }
-    cout << wins;
+    cout << wins << endl;
     return 0;
 }
--- USACOClassGold/src/yetanothernocross.cpp
+++ USACOClassGold/src/yetanothernocross.cpp
@@ -25,10 +25,23 @@ int main(int argc, char const *argv[])
     }
     // r  l
     dp[0][0] = 0;
+    dp[0][1] = 0;
+    dp[1][0] = 0;
+    dp[1][1] = 0;
     for(int i = 0; i < N; i ++){
         for(int j = 0; j < N; j ++){
             // we choose the next on the right
             // and find ranges on the left
+            
+            if(i > 0 && j > 0){
+                dp[i][j] = max(dp[i][j],dp[i - 1][j - 1]);
+            }
+            if(i >= 1 && j >= 0){
+                dp[i][j] = max(dp[i - 1][j],dp[i][j]);
+            }
+            if(i >= 0 && j >= 1){
+                dp[i][j] = max(dp[i][j],dp[i][j - 1]);
+            }
             if(dp[i][j] == -1){
                 continue; // skip impossible/unreachable
             }
@@ -40,12 +53,10 @@ int main(int argc, char const *argv[])
                 if(leftIndex < j){
                     continue;
                 }
-                // next usable i,j starting point
-                for(int l = i + 1; l <= N; l ++){
-                    // apply to col
-                    // l: row num
-                    dp[l][leftIndex + 1] = max(dp[l][leftIndex + 1], dp[i][j] + 1);
-                }
+                dp[i + 1][leftIndex + 1] = max(dp[i + 1][leftIndex + 1], dp[i][j] + 1);
+                dp[i + 1][leftIndex + 2] = max(dp[i + 1][leftIndex + 2], dp[i][j] + 1);
+                dp[i + 2][leftIndex + 1] = max(dp[i + 2][leftIndex + 1], dp[i][j] + 1);
+                dp[i + 2][leftIndex + 2] = max(dp[i + 2][leftIndex + 2], dp[i][j] + 1);
             }
         }
     }
