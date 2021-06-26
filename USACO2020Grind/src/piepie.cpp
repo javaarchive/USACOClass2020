7b3082f9907566ed73d78823eed17c631cad8ea9
Raymond <javaarchive@repl.email>
2021-06-25 22:16:58 -0700

    major refactor

--- USACO2020Grind/src/piepie.cpp
+++ USACO2020Grind/src/piepie.cpp
@@ -9,20 +9,56 @@
 
 using namespace std;
 
-int N,D;
+int N;
+long long D;
 struct Pie{
     int index; // Original index
     long long bessieTaste;
     long long elsieTaste;
 };
-map<long long,vector<Pie>> bPies;
-map<long long,vector<Pie>> ePies;
-multiset<long long> bTasteVals;
-multiset<long long> eTasteVals;
+// vector<Pie> Bs;
+// vector<Pie> Es;
+/*struct cmpE
+{
+	bool operator()(int a,int b) const
+	{
+		return Es[a].bessieTaste < Es[b].bessieTaste;
+	}
+};
+struct cmpB
+{
+	bool operator()(int a,int b) const
+	{
+		return Bs[a].elsieTaste < Bs[b].elsieTaste;
+	}
+};*/
+struct cmpB
+{
+	bool operator()(const Pie a,const Pie b) const
+	{
+		return a.bessieTaste < b.bessieTaste;
+	}
+};
+struct cmpE
+{
+	bool operator()(const Pie a,const Pie b) const
+	{
+		return a.elsieTaste < b.elsieTaste;
+	}
+};
+// map<long long,vector<Pie>> bPies;
+// map<long long,vector<Pie>> ePies;
+// multiset<long long> bTasteVals;
+// multiset<long long> eTasteVals;
+multiset<int> bTasteINDEXES; // best elsie taste of bessies
+multiset<int> eTasteINDEXES; // best bessie taste of elsies
 int shortestPathB[MAXN] = {INT32_MAX};
 int shortestPathE[MAXN] = {INT32_MAX};
 vector<Pie> bessieDeadEnds; // where e=0
 vector<Pie> elsieDeadEnds; // where b=0
+
+multiset<Pie,cmpE> bessiePies;
+multiset<Pie,cmpB> elsiePies;
 
 
 struct State{
@@ -44,9 +80,66 @@ queue<State> pq;
 multiset<long long>::iterator bs(const multiset<long long>& st,long long target){
     auto exactIT = st.find(target);
     if(exactIT != st.end()){
+        // cout << "bs(" << target << ") -> " << " exact found";
         return exactIT;
     }
-    return st.upper_bound(target);
+    auto lb = st.lower_bound(target);
+    if(lb == st.begin()){
+        return st.end();
+    }else{
+        return --lb;
+    }
+    //cout << "bs(" << target << ") -> " << " lb: " << (((--st.lower_bound(target)) == st.end())?-1:*st.upper_bound(target)) << endl;
+    //return st.upper_bound(target);
+}
+/*multiset<Pie>::iterator bs2(const multiset<Pie>& st,Pie target){
+    auto exactIT = st.find(target);
+    if(exactIT != st.end()){
+        // cout << "bs2(" << target << ") -> " << " exact found";
+        return exactIT;
+    }
+    auto lb = st.lower_bound(target);
+    if(lb == st.begin()){
+        return st.end();
+    }else{
+        return -- lb;
+    }
+    //cout << "bs(" << target << ") -> " << " lb: " << (((--st.lower_bound(target)) == st.end())?-1:*st.upper_bound(target)) << endl;
+    //return st.upper_bound(target);
+}*/
+
+multiset<Pie,cmpB>::iterator bs3(const multiset<Pie,cmpB>& st,Pie target){
+    // cout << "bs2(" << target.bessieTaste << ")" << endl;
+    /*auto exactIT = st.find(target);
+    if(exactIT != st.end()){
+        // cout << "bs2(" << target << ") -> " << " exact found";
+        return exactIT;
+    }*/
+    auto lb = st.lower_bound(target);
+    if(lb == st.begin()){
+        // cout << "bs targeted beginning at " << (*lb).bessieTaste << endl;
+        return st.end();
+    }else{
+        return -- lb;
+    }
+    //cout << "bs(" << target << ") -> " << " lb: " << (((--st.lower_bound(target)) == st.end())?-1:*st.upper_bound(target)) << endl;
+    //return st.upper_bound(target);
+}
+
+multiset<Pie,cmpE>::iterator bs4(const multiset<Pie,cmpE>& st,Pie target){
+    /*auto exactIT = st.find(target);
+    if(exactIT != st.end()){
+        // cout << "bs2(" << target << ") -> " << " exact found";
+        return exactIT;
+    }*/
+    auto lb = st.lower_bound(target);
+    if(lb == st.begin()){
+        return st.end();
+    }else{
+        return --lb;
+    }
+    //cout << "bs(" << target << ") -> " << " lb: " << (((--st.lower_bound(target)) == st.end())?-1:*st.upper_bound(target)) << endl;
+    //return st.upper_bound(target);
 }
 
 void dij(){
@@ -80,16 +173,22 @@ void dij(){
         // Explore other options
         if(!st.isBessiePie){
             // Find a reason for bessie to gift elsie this
-            bool signalEnd = false;
-            for(auto iter = bs(eTasteVals,st.p.elsieTaste); !signalEnd; iter --){ // Going backwards !!
+            // bool signalEnd = false;
+            // find a pie of bessie's that tastes similar in elsie POV
+            // for(auto iter = bs(eTasteVals,st.p.elsieTaste); !signalEnd; iter --){ // Going backwards !!
             //auto iter = bs(eTasteVals,st.p.elsieTaste);
-                if(iter == eTasteVals.end()){
+            auto iter = bs4(bessiePies,st.p);
+            while(true){
+                
+                if(iter == bessiePies.end()){
                     break;
-                }else if(iter == eTasteVals.begin()){
-                    signalEnd = true;
                 }
-                    long long taste = *iter;
-                    // cout << "Checking taste value " << taste << endl;
+                
+                //else if(iter == eTasteVals.begin()){
+                //    signalEnd = true;
+                // }
+                    long long taste = (*iter).bessieTaste;
+                    //cout << "Checking taste value " << taste << endl;
                     if((st.p.elsieTaste - taste) > D){
                         // out of range
                         break;
@@ -97,12 +196,15 @@ void dij(){
 
                     // Loop the list
                     // cout << "All like " << taste << endl;
-                    for(Pie newp: bPies[taste]){
+                    // for(Pie newp: bPies[taste]){
                         // Check if we've visited this already
+                        Pie newp = *iter;
                         if(visitedB[newp.index]){
+                            iter --;
                             continue;
                         }
                         if((st.depth + 1) >= shortestPathB[newp.index]){
+                            iter --;
                             continue; // skip
                         }
                         // Otherwise, add it to explore
@@ -114,16 +216,17 @@ void dij(){
                         visitedB[newp.index] = true;
                         // cout << "++ " << newp.bessieTaste << "," << newp.elsieTaste << endl;
                         pq.push(newSt);
-                        signalEnd = true;
-                        eTasteVals.erase(taste);
-                        break;
-                    }
-                    
+                        // signalEnd = true;
+                        elsiePies.erase(newp);
+                        // break;
+                    // }
             }
+            // }
         }else{
             // Find a reason for elsie to gift bessie this
-            bool signalEnd = false;
-            for(auto iter = bs(bTasteVals, st.p.bessieTaste); !signalEnd; iter --){ // Going backwards !!
+            // bool signalEnd = false;
+            // find a pie of elsie's that tastes similar in bessie POV
+            /*for(auto iter = bs(bTasteVals, st.p.bessieTaste); !signalEnd; iter --){ // Going backwards !!
                 if(iter == bTasteVals.end()){
                     break;
                     //signalEnd = true; // about to end
@@ -138,13 +241,32 @@ void dij(){
                 }
 
                 // Loop the list
-                for(Pie newp: ePies[taste]){
+                
+                for(Pie newp: ePies[taste]){*/
                     // Check if we've visited this already
+                auto iter = bs3(elsiePies, st.p);
+                while(true){
+
+                    
+                    if(iter == elsiePies.end()){
+                        // cout << "ERR: Not found " << elsiePies.size() << endl;
+                        break;
+                    }
+                    
+                    Pie newp = *iter;
+                    long long taste = newp.elsieTaste;
+
                     if(visitedE[newp.index]){
+                        iter --;
                         continue;
                     }
                     if((st.depth + 1) >= shortestPathE[newp.index]){
+                        iter --;
                         continue; // skip
+                    }
+                    if((st.p.elsieTaste - taste) > D){
+                        // out of range
+                        break;
                     }
                     // Otherwise, add it to explore
                     State newSt;
@@ -155,11 +277,12 @@ void dij(){
                     visitedE[newp.index] = true;
                     // cout << "++ " << newp.bessieTaste << "," << newp.elsieTaste << endl;
                     pq.push(newSt);
-                    signalEnd = true;
-                    bTasteVals.erase(taste);
-                    break;
+                    // signalEnd = true;
+                    bessiePies.erase(newp);
+                    // break;
+                //}
+            // }
                 }
-            }
         }
     }
 }
@@ -180,9 +303,12 @@ int main(int argc, char const *argv[])
         Pie p;
         cin >> p.bessieTaste >> p.elsieTaste;
         p.index = i;
-        bPies[p.elsieTaste].push_back(p);
-        bTasteVals.insert(p.bessieTaste);
-        eTasteVals.insert(p.elsieTaste);
+        //Bs.push_back(p);
+        // bPies[p.elsieTaste].push_back(p);
+        // Bindexes.insert(i);
+        // bTasteVals.insert(p.bessieTaste);
+        // eTasteVals.insert(p.elsieTaste); // orig
+        bessiePies.insert(p);
         if(p.elsieTaste == 0){
             bessieDeadEnds.push_back(p);
         }
@@ -191,9 +317,12 @@ int main(int argc, char const *argv[])
         Pie p;
         cin >> p.bessieTaste >> p.elsieTaste;
         p.index = i;
-        ePies[p.bessieTaste].push_back(p);
-        bTasteVals.insert(p.bessieTaste);
-        eTasteVals.insert(p.elsieTaste);
+        // Es.push_back(p);
+        // ePies[p.bessieTaste].push_back(p);
+        // Eindexes.insert(i);
+        // bTasteVals.insert(p.bessieTaste); // orig
+        // eTasteVals.insert(p.elsieTaste);
+        elsiePies.insert(p);
         if(p.bessieTaste == 0){
             elsieDeadEnds.push_back(p);
         }
