cad613d83763c522667f4b913ca540687b3c59f2
Raymomyar <javaarchive@repl.email>
2021-08-05 22:39:56 -0700

    ugh

--- cforces/src/mikasa.cpp
+++ cforces/src/mikasa.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
+#include <cmath>
+#include <bitset>
 // really strange problem name tbh
 using namespace std;
+int getBit(int input, int i){
+    return (input % (1 << (i+1))) >= (1 << i); 
+}
+void solve(){
+    int n,m;
+    cin >> n >> m;
+    // cout << "M: " << bitset<8>(m) << endl; 
+    // cout << "M + 1: " << bitset<16>(m + 1) << endl; 
+    // cout << "N    : " << bitset<16>(n) << endl; 
+    // naive solution old
+    /*for(int i = 0; i < INT32_MAX; i++){
+        
+        // cout << n << "^" << i << " = " << (n^i) << " > " << m << endl;
+        if((n ^ i) > m){
+            cout << i << endl;
+            return;
+        }
+    }*/
+    int out = 0;
+    int upBound = max(ceil(log2(n)),ceil(log2(m + 1)));
+    for(int i = upBound ; i >= 0; i --){
+        int bitM1 = getBit(m + 1, i);
+        int bitN  = getBit(n    , i);
+        // cout << bitM1 << " and " << bitN << endl;
+        if(bitN == 0 && bitM1 == 1){
+            out += 1 << i;
+        }
+        if(bitN == 1 && bitM1 == 0){
+            // power is greater than everything else to the right of it
+            break;
+        }
+    }
+    cout << out << endl;
+}
+int main(int argc, char const *argv[])
+{
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; i ++){
+        solve();
+    }
+    return 0;
+}
---
+++ cforces/src/twotables.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+ *      N
+ *     ---
+ *  W |   |
+ *    |   | E
+ *     ---
+ *      S
+ */
+void solve(){
+    int W,H;
+    cin >> W >> H;
+    int x1,y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int w,h;
+    cin >> w >> h;
+
+    // obviously
+    if(w > W || h > H){
+        // sanity
+        // cout << "SANITY";
+        cout << -1 << endl;
+    }
+
+    // Distances from walls
+    int NORTHERN_DIST = H - y2;
+    int SOUTHERN_DIST = y1;
+    int EASTERN_DIST  = W - x2;
+    int WESTERN_DIST  = x1;
+
+    vector<pair<int,int>> choices;
+    choices.push_back(make_pair(0, h - NORTHERN_DIST));
+    choices.push_back(make_pair(1, h - SOUTHERN_DIST));
+    choices.push_back(make_pair(2,  w - EASTERN_DIST));
+    choices.push_back(make_pair(3,  w - WESTERN_DIST));
+    // works anyway
+    if(NORTHERN_DIST >= h || SOUTHERN_DIST >= h){
+        cout << 0 << endl;
+        return;
+    }
+    if(WESTERN_DIST >= w || EASTERN_DIST >= w){
+        cout << 0 << endl;
+        return;
+    }
+    
+
+
+    sort(choices.begin(), choices.end(), [](const pair<int,int>& a, const pair<int,int>& b){
+        /*int predictA = 0, predictB = 0;
+        if(a.first == 0 || a.first == 1){
+            predictA = h - a.second;
+        }
+        return predictA < predictB;*/
+        return a.second > b.second;
+        // return a.second > b.second;
+    });
+    cout << "PRE: ";
+    // cout << endl << "SORT: " << choices[0].second << " , " << choices[1].second << choices[2].second << choices[3].second << endl;
+    
+    for(int i = 0; i < 4; i ++){
+        pair<int,int> choice = choices[i];
+        int id = choice.first;
+        // bool worked = false;
+        switch(id){
+            case 0:
+                // headroom check
+                if((h - NORTHERN_DIST) <= SOUTHERN_DIST){
+                    cout << (h - NORTHERN_DIST) << endl;
+                    return;
+                }
+                break;
+            case 1:
+                if((h - SOUTHERN_DIST) <= NORTHERN_DIST){
+                    cout << (h - SOUTHERN_DIST) << endl;
+                    return;
+                }
+                break;
+
+            case 2:
+                if((w - EASTERN_DIST) <= WESTERN_DIST){
+                    cout << (w - EASTERN_DIST) << endl;
+                    return;
+                }
+                break;
+
+            case 3:
+                if((w - WESTERN_DIST) <= EASTERN_DIST){
+                    cout << (w - WESTERN_DIST) << endl;
+                    return;
+                }
+                break;
+        
+        }
+        /*if(worked){
+            break;
+        }*/
+    }
+    cout << -1 << endl;     
+}
+
+int main(int argc, char const *argv[])
+{
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i ++){
+        solve();
+    }
+    return 0;
+}
