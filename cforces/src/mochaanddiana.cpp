5619e744136e0c40c740978c7acd53bce495fb30
Raymomyar <javaarchive@repl.email>
2021-08-22 21:25:43 -0700

    low mem mocha and diana

--- cforces/src/mochaanddiana.cpp
+++ cforces/src/mochaanddiana.cpp
@@ -1,55 +1,134 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
 #include <vector>
-#define MAXN 1002
+#define MAXN 100002
 
 using namespace std;
+
+pair<int,int> normalize(int a,int b){
+    return make_pair(min(a,b),max(a,b));
+}
 
 void solve(){
     int n, m1, m2;
     cin >> n >> m1 >> m2;
-    set<int> mocha[MAXN];
-    set<int> diana[MAXN];
+    map<int,vector<int>> mocha;
+    map<int,vector<int>> diana;
+    // bool existing[MAXN][MAXN];
+    set<pair<int,int>> existing;
     for(int i = 0; i < m1; i ++){
         int a,b;
         cin >> a >> b;
-        mocha[a].insert(b);
-        mocha[b].insert(a);
+        mocha[a].push_back(b);
+        mocha[b].push_back(a);
+        existing.insert(normalize(a,b));
     }
     for(int i = 0; i < m2; i ++){
         int a,b;
         cin >> a >> b;
-        diana[a].insert(b);
-        diana[b].insert(a);
+        diana[a].push_back(b);
+        diana[b].push_back(a);
+        existing.insert(normalize(a,b));
     }
-    vector<pair<int,int>> out;
-    for(int i = 1; i <= n; i ++){
-        for(int j = 1; j < i; j ++){
-            // vector<int>::iterator it;
-            vector<int> intersection;
-            set_intersection(mocha[i].begin(), mocha[i].end(), mocha[j].begin(), mocha[j].end(), back_inserter(intersection));
-            if(intersection.size() > 0){
-                continue; // common so triangle
-            }
-            set_intersection(diana[i].begin(), diana[i].end(), diana[j].begin(), diana[j].end(), back_inserter(intersection));
-            if(intersection.size() > 0){
-                continue; // common so triangle
-            }
-            if(mocha[i].find(j) != mocha[i].end() || diana[i].find(j) != diana[i].end()){
-                continue; // already exists
-            }
-            out.push_back(make_pair(i,j));
-            diana[i].insert(j);
-            diana[j].insert(i);
 
-            mocha[i].insert(j);
-            mocha[j].insert(i);
+    int srcm[MAXN] = {0};
+    int srcd[MAXN] = {0};
+    fill(srcm, srcm + MAXN,0);
+    fill(srcd, srcd + MAXN,0);
+    for(int i = 1; i <= n; i ++){
+        if(srcm[i] == 0){
+            // dfs
+            stack<int> next;
+            next.push(i);
+            srcm[i] = i;
+            while(!next.empty()){
+                int cur = next.top();
+                next.pop();
+                for(int j = 0; j < mocha[cur].size(); j ++){
+                    int nextNode = mocha[cur][j];
+                    if(srcm[nextNode] == 0){
+                        srcm[nextNode] = i; // master node
+                        next.push(nextNode);
+                    }
+                }
+            }
         }
     }
-    cout << out.size() << endl;
-    for(auto p: out){
-        cout << p.first << " " << p.second << endl;
+    
+    for(int i = 1; i <= n; i ++){
+        if(srcd[i] == 0){
+            // dfs
+            stack<int> next;
+            next.push(i);
+            srcd[i] = i;
+            while(!next.empty()){
+                int cur = next.top();
+                next.pop();
+                for(int j = 0; j < diana[cur].size(); j ++){
+                    int nextNode = diana[cur][j];
+                    if(srcd[nextNode] == 0){
+                        srcd[nextNode] = i; // master node
+                        next.push(nextNode);
+                    }
+                }
+            }
+        }
+    }
+    vector<pair<int,int>> out;
+    int ans = 0;
+    for(int i = 1; i <= n; i ++){
+        for(int j = i + 1; j <= n; j ++){
+            if(srcm[i] == srcm[j] || srcd[i] == srcd[j] || existing.find(normalize(i,j)) != existing.end()){
+                continue;
+            }
+            out.push_back(make_pair(i,j));
+            existing.insert(normalize(i,j));
+            int srcA = srcm[i];
+            stack<int> next;
+            next.push(j);
+            srcm[j] = srcA;
+            // cout << "dfsing" << endl;
+            while(!next.empty()){
+                int cur = next.top();
+                next.pop();
+                for(int k = 0; k < mocha[cur].size(); k ++){
+                    int nextNode = mocha[cur][k];
+                    if(srcm[nextNode] != srcA){
+                        srcm[nextNode] = srcA;
+                        next.push(nextNode);
+                    }
+                }
+            }
+            int srcB = srcd[i];
+            next.push(j);
+            srcd[j] = srcB;
+            while(!next.empty()){
+                int cur = next.top();
+                next.pop();
+                for(int k = 0; k < diana[cur].size(); k ++){
+                    int nextNode = diana[cur][k];
+                    if(srcd[nextNode] != srcB){
+                        srcd[nextNode] = srcB;
+                        next.push(nextNode);
+                    }
+                }
+            }
+            mocha[i].push_back(j);
+            mocha[j].push_back(i);
+            diana[i].push_back(j);
+            diana[j].push_back(i);
+
+            ans ++;
+        }
+    }
+    // ans -= m1 + m2;
+    cout << ans << endl;
+    for(auto pairing: out){
+        cout << pairing.first << " " << pairing.second << endl;
     }
 }
 
