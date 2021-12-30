3e606a5abca02c232bca3258c9725dcba47d175c
Raymomyar <javaarchive@repl.email>
2021-12-29 17:14:59 -0800

    before dfs change

--- USACOContest/connecting_2_barns_vanilla.cpp
+++ USACOContest/connecting_2_barns_vanilla.cpp
@@ -7,8 +7,10 @@ using namespace std;
 bool visited[MAXN] = {false};
 int comp[MAXN] = {-1};
 map<int,vector<int>> graph;
+vector<vector<int>> compNodes = vector<vector<int>>(MAXN);
+stack<int> nextNode;
 
-int getSmallestDiff(vector<int>& a, vector<int>& b){
+int getSmallestDiff(int a, int b){
     
     int smallest = INT32_MAX;
     int i = 0, j = 0;
@@ -18,12 +20,12 @@ int getSmallestDiff(vector<int>& a, vector<int>& b){
     for(auto elem: b){cout << elem << " ";}
     cout << endl;*/
     
-    while(i < a.size() && j < b.size()){
-        int diff = abs(a[i] - b[j]);
+    while(i < compNodes[a].size() && j < compNodes[b].size()){
+        int diff = abs(compNodes[a][i] - compNodes[b][j]);
         if(diff < smallest){
             smallest = diff;
         }
-        if(a[i] < b[j]){
+        if(compNodes[a][i] < compNodes[b][j]){
             i ++;
         }else{
             j ++;
@@ -41,7 +43,9 @@ void solve(){
     cin >> N >> M;
     fill(comp, comp + MAXN, -1);
     fill(visited, visited + N, false);
-    vector<vector<int>> compNodes(N + 2);
+    for(int i = 0; i <= N; i ++){
+        compNodes[i].clear();
+    }
     /*for(int i = 0; i < N; i ++){
         compNodes.push_back(vector<int>());
     }*/
@@ -58,7 +62,7 @@ void solve(){
     // cout << endl;
     int compID = 1;
     // cout << "DFSing" << endl;
-    stack<int> next;
+
     for(int i = 0; i < N; i ++){
         if(visited[i]){
             continue; // skip already visited
@@ -67,11 +71,11 @@ void solve(){
         visited[i] = true;
         comp[i] = compID;
         // cout << "DFSing from " << i << " for comp " << compID << endl;
-        next.push(i);
+        nextNode.push(i);
         compNodes[compID].push_back(i);
-        while(!next.empty()){
-            int node = next.top();
-            next.pop();
+        while(!nextNode.empty()){
+            int node = nextNode.top();
+            nextNode.pop();
             for(int otherNode: graph[node]){
                 if(visited[otherNode]){
                     continue; // already visited
@@ -82,7 +86,7 @@ void solve(){
                 visited[otherNode] = true;
                 // cout << "Marked " << otherNode << " as " << compID << endl;
                 compNodes[compID].push_back(otherNode);
-                next.push(otherNode);
+                nextNode.push(otherNode);
             }
         }
         compID ++;
@@ -114,12 +118,12 @@ void solve(){
         cout << 0 << endl;
     }else{
          // single connection required
-        long long sq_root = getSmallestDiff(compNodes[comp[0]],compNodes[comp[N - 1]]);
+        long long sq_root = getSmallestDiff(comp[0],comp[N - 1]);
         // cout << (sqrt*sqrt) << endl;
         long long best = sq_root * sq_root;
         for(int i = 2; i < (compID - 1); i ++){
-            long long distA = getSmallestDiff(compNodes[1],compNodes[i]);
-            long long distB = getSmallestDiff(compNodes[i],compNodes[comp[N - 1]]);
+            long long distA = getSmallestDiff(1,i);
+            long long distB = getSmallestDiff(i,comp[N - 1]);
             long long cost = distA * distA + distB * distB;
             // cout << "1 -> " << i << " -> " << " : " << distA * distA << " + " << distB * distB << " = " << cost << endl;
             best = min(best, cost);
